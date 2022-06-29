0c19fd8171f1693d8ee0077286ddd6ff3cea7a1c
umbrik <idumbrik@gmail.com>
2022-06-29 13:05:22 +0300

    add education program declaration

--- lib/xml/education_plan/index.d.ts
+++ lib/xml/education_plan/index.d.ts
@@ -1,4 +1,3 @@
-
 interface IWTEducationPlanTopElem extends IWTDocInfo,
   ObjectCodeNameBase,
   IWTPersonFillingBase,
---
+++ lib/xml/education_program/index.d.ts
@@ -0,0 +1,16 @@
+interface IWTEducationProgramEducationMethod {
+  education_method_id?: XmlElem<number>;
+}
+
+interface IWTEducationProgramTopElem extends IWTAdminAccessBase {
+  code?: XmlElem<string>;
+	name?: XmlElem<string>;
+	desc?: XmlElem<string>;
+  education_methods?: XmlMultiElem<IWTEducationProgramEducationMethod>;
+	comment?: XmlElem<string>;
+	doc_info?: XmlElem<IWTDocInfoBase>;
+	access?: XmlElem<IWTAccessDocBase>;
+	role_id?: XmlMultiElem<number>;
+}
+
+type IWTEducationProgramDocument = IWTXmlDocument<IWTEducationProgramTopElem>;
