f8511b1e51be9cdc4773e3d9fdbc783a7da72fc2
umbrik <idumbrik@gmail.com>
2022-07-04 16:07:01 +0300

    move all SA vars to SA xml declaration

--- lib/xml/server_agent/index.d.ts
+++ lib/xml/server_agent/index.d.ts
@@ -1,3 +1,15 @@
+/** Объект параметров агента */
+declare var Param: Object;
+/** ID объекта над которым выполняется агент */
+declare var OBJECT_ID: number;
+declare var OBJECTS_ID_STR: string;
+/** Флаг исполнения кода на клиенте */
+declare var LdsIsClient: boolean;
+/** Флаг исполнения кода на сервере */
+declare var LdsIsServer: boolean;
+
+declare var oData: any;
+
 interface IWTServerAgentTopElem {
   id?: XmlElem<number>;
   code?: XmlElem<string>;
