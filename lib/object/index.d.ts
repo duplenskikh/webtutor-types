e3bf9ccd15ed2160c0bfb63f2d6e016bbc0310fd
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-01 18:37:03 +0300

    upd object type

--- lib/object/index.d.ts
+++ lib/object/index.d.ts
@@ -1,28 +1,56 @@
 /**
- * Тип объект в системе WebTutor HCM
+ * Конструктор объекта javascript. Создает новый объект.
+ * По умолчанию для объекта определен атрибут HasProperty и методы
+ * AddProperty , SetProperty , GetProperty , GetOptProperty .
  */
 interface Object {
-  [key: string]: any;
+  [key: string]: unknown;
+
+  /**
+   * Выдает true, если атрубут с заданным наименованием существует,
+   * и false - если не существует. Смотри также GetOptProperty, 
+   * GetOptProperty и SetProperty.
+   * @param key наименование атрибута объекта
+   */
+  HasProperty(key: string): boolean;
+
+  /**
+   * Добавляет в объект (javascript object) новый атрибут, и присваивает ему значение.
+   * @param key наименование атрибута (string)
+   * @param name значение атрибута (variant)
+   */
+  AddProperty(key: string, name: unknown): void;
 
   /**
    * Возвращает значение атрибута объекта. Если атрубут отсутствует, выдает undefined.
-   * @param {string} propertyName - наименование атрибута объекта.
-   * @param defaultValue - значение по умолчани, возращаемое в случае отстутвия атрибута (Any). Необязятельный аргумент. По умолчанию равен undefined.
-   * @returns any
+   * @param {string} key - наименование атрибута объекта.
+   * @param defaultValue - значение по умолчанию, возращаемое в случае
+   * отстутвия атрибута (Any). Необязятельный аргумент. По умолчанию равен undefined.
    */
-  GetOptProperty?(propertyName: string, defaultPropertyValue?: any): any;
+  GetOptProperty(key: string, defaultValue?: unknown): unknown | undefined;
 
-  GetProperty?(propertyName: string): any;
+  /**
+   * Выдает значение атрибута объекта. Если атрибут отсутствует, выдает ошибку.
+   * Смотри также GetOptProperty() и SetProperty.
+   * @param key наименование атрибута объекта
+   * @example AppConfig.GetProperty( 'alt-app-name' );
+   * @throws {Error}
+   */
+  GetProperty(key: string): unknown | never;
 
-  SetProperty?(propertyName: string, propertyValue: any): void;
-
-  HasProperty?(propertyName: string): boolean;
+  /**
+   * Устанавливает значение атрибута объекта. Если атрубут отсутствует,
+   * добавляет его. Смотри также GetOptProperty() и GetProperty.
+   * @param key наименование атрибута объекта.
+   * @param value значение атрибута объекта.
+   */
+  SetProperty(key: string, value: any): void;
 }
 
 interface ObjectConstructor {
   new(value?: any): Object;
-  (): Object;
   (value?: any): Object;
+  (): Object;
 }
 
-declare var Object: ObjectConstructor;
+declare const Object: ObjectConstructor;
