f4e2b26cf80104b713c9cb6ed5b4ba0c0935fbd4
umbrik <idumbrik@gmail.com>
2022-08-01 18:09:45 +0300

    upd math

--- lib/math/index.d.ts
+++ lib/math/index.d.ts
@@ -1,5 +1,17 @@
-declare namespace Math {
-  function round(number: number): number;
-  function sqrt(number: number): number;
-  function ceil(number: number): number;
+interface Math {
+  /**
+   * Округление значения числового выражения к ближайшему целому.
+   * @param number - число, округляемое до ближайшего целого (Real).
+   */
+  round(number: number): number;
+
+  /**
+   * Возвращает квадратный корень числа .
+   * @param number числовое выражение (Real).
+   */
+  sqrt(number: number): number;
+
+  ceil(number: number): number;
 }
+
+declare const Math: Math;
