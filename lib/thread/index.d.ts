b0a848a3f47b3706e3ac29c1d40f4d81c310e534
umbrik <idumbrik@gmail.com>
2022-08-01 18:51:15 +0300

    rework thread types

--- lib/thread/index.d.ts
+++ lib/thread/index.d.ts
@@ -1,8 +1,35 @@
 interface Thread {
+  /**
+   * Проверяет, выполняется ли еще запущенный поток.
+   */
   IsRunning: boolean;
+
+  /**
+   * Объект типа Object, используемый для хранения произвольных параметров.
+   * Обычно параметры устанавливаются перед запуском потока, после чего код,
+   * выполняемый в потоке, использует их.
+   * 
+   * Параметр не является thread-safe, поэтому код, запускающий поток,
+   * не должен обращаться к ним после запуска потока.
+   */
   Param: Object;
 
-  EvalCode(code: string): any;
+  /**
+   * Запускает выполнения потока из заданного кода.
+   * @param code строка, содержащая код (String).
+   * @xample
+   * thread = new Thread;
+   * thread.EvalCode( 'lib_backup.run_backup()' );
+   */
+  EvalCode(code: string): void;
+
+  /**
+   * Запускает выполнения потока из кода, загруженного из заданного url.
+   * @param url url, содержащий код (String).
+   * @example
+   * thread = new Thread;
+   * thread.EvalCodeUrl( 'rcr_agent.js' );
+   */
   EvalCodeUrl(url: string): any;
 }
 
@@ -11,4 +38,10 @@ interface ThreadConstructor {
   (...arg: any): Thread;
 }
 
+/**
+ * Объект, обозначающий отдельный запускаемый поток (thread).
+ * На сегодняшний день создание потока через объект Thread не рекомендуется,
+ * поскольку существует более удобный способ запyска потока через метод EvalThread()
+ * объекта XmlDoc, предлагающий концепцию класса, содержажего атрибуты и методы.
+ */
 declare const Thread: ThreadConstructor;
