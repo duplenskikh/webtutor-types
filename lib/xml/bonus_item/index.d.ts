99c02a18b9129b2fba918ae69db88683fe9a183d
umbrik <idumbrik@gmail.com>
2022-08-03 10:20:52 +0300

    ref bonus_item

--- lib/xml/bonus_item/index.d.ts
+++ lib/xml/bonus_item/index.d.ts
@@ -1,15 +1,27 @@
-type BonusItemDocumentTopElem = XmlTopElem & { Doc: BonusItemDocument } & 
-AdminAccessBase & {
-  name?(): string;
-  assessment_appraise_id?: XmlElem<number>;
-  budget_period_id?: XmlElem<number>;
-  person_id?: XmlElem<number>;
-  period_start?: XmlElem<Date>;
-  period_end?: XmlElem<Date>;
-  result?: XmlElem<number>;
-  calc_result?(): unknown;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+interface BonusItemDocumentBonuse {
+  pa_id: XmlElem<number>;
+  bonus_profile_id: XmlElem<number>;
+  value: XmlElem<number>;
+  boss_treat: XmlElem<number>;
+  overall: XmlElem<number>;
 }
 
-type BonusItemDocument = XmlDocument & { TopElem: BonusItemDocumentTopElem; };
+type BonusItemDocumentTopElem = XmlTopElem &
+AdminAccessBase & {
+  Doc: BonusItemDocument;
+  name(): string;
+  assessment_appraise_id: XmlElem<number>;
+  budget_period_id: XmlElem<number>;
+  person_id: XmlElem<number>;
+  period_start: XmlElem<Date>;
+  period_end: XmlElem<Date>;
+  bonuses: XmlMultiElem<BonusItemDocumentBonuse>;
+  result: XmlElem<number>;
+  calc_result(): void;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+}
+
+type BonusItemDocument = XmlDocument & {
+  TopElem: BonusItemDocumentTopElem;
+};
