d559c711a9ca44b122819387ba2c59a98578d073
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 10:44:51 +0300

    ref change

--- lib/xml/change/index.d.ts
+++ lib/xml/change/index.d.ts
@@ -1,14 +1,17 @@
-type ChangeDocumentTopElem = XmlTopElem & { Doc: ChangeDocument } & {
-  name?: XmlElem<string>;
-  create_date?: XmlElem<Date>;
-  person_id?: XmlElem<number>;
-  person_fullname?: XmlElem<string>;
-  object_id?: XmlElem<number>;
-  object_type?: XmlElem<string>;
-  object_name?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  desc?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+type ChangeDocumentTopElem = XmlTopElem & {
+  Doc: ChangeDocument;
+  name: XmlElem<string>;
+  create_date: XmlElem<Date>;
+  person_id: XmlElem<number>;
+  person_fullname: XmlElem<string>;
+  object_id: XmlElem<number>;
+  object_type: XmlElem<string>;
+  object_name: XmlElem<string>;
+  comment: XmlElem<string>;
+  desc: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type ChangeDocument = XmlDocument & { TopElem: ChangeDocumentTopElem; };
+type ChangeDocument = XmlDocument & {
+  TopElem: ChangeDocumentTopElem;
+};
