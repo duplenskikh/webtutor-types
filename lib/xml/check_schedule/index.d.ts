54a0d1f31530b9609b4e5ed745718fa91269a77d
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 10:53:37 +0300

    ref check_schedule

--- lib/xml/check_schedule/index.d.ts
+++ lib/xml/check_schedule/index.d.ts
@@ -1,16 +1,19 @@
-type CheckScheduleDocumentTopElem = XmlTopElem & { Doc: CheckScheduleDocument } & 
+type CheckScheduleDocumentTopElem = XmlTopElem &
 AdminAccessBase &
 WebVariablesBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  is_enabled?: XmlElem<boolean>;
-  type_id?: XmlElem<string>;
-  library_url?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  is_std?: XmlElem<boolean>;
-  changed?: XmlElem<boolean>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: CheckScheduleDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  is_enabled: XmlElem<boolean>;
+  type_id: XmlElem<string>;
+  library_url: XmlElem<string>;
+  comment: XmlElem<string>;
+  is_std: XmlElem<boolean>;
+  changed: XmlElem<boolean>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type CheckScheduleDocument = XmlDocument & { TopElem: CheckScheduleDocumentTopElem; };
+type CheckScheduleDocument = XmlDocument & {
+  TopElem: CheckScheduleDocumentTopElem;
+};
