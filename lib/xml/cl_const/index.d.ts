706d865fd7bb69ef7c9c55f09e8bddc7db8b446c
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 10:55:18 +0300

    ref cl_const

--- lib/xml/cl_const/index.d.ts
+++ lib/xml/cl_const/index.d.ts
@@ -1,31 +1,34 @@
 interface ClConstDocumentLocalization {
-  id?: XmlElem<number>;
-  text?: XmlElem<string>;
-  imprint?: XmlElem<string>;
+  id: XmlElem<number>;
+  text: XmlElem<string>;
+  imprint: XmlElem<string>;
 }
 
 interface ClConstDocumentObjectTypeObject {
-  id?: XmlElem<number>;
+  id: XmlElem<number>;
 }
 interface ClConstDocumentObjectType {
-  id?: XmlElem<string>;
-  objects?: XmlMultiElem<ClConstDocumentObjectTypeObject>;
+  id: XmlElem<string>;
+  objects: XmlMultiElem<ClConstDocumentObjectTypeObject>;
 }
 
 interface ClConstDocumentFlag {
-  id?: XmlElem<string>;
+  id: XmlElem<string>;
 }
 
-type ClConstDocumentTopElem = XmlTopElem & { Doc: ClConstDocument } & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  localizations?: XmlMultiElem<ClConstDocumentLocalization>;
-  object_types?: XmlMultiElem<ClConstDocumentObjectType>;
-  flags?: XmlMultiElem<ClConstDocumentFlag>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  set_value?(): unknown;
+type ClConstDocumentTopElem = XmlTopElem & {
+  Doc: ClConstDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  localizations: XmlMultiElem<ClConstDocumentLocalization>;
+  object_types: XmlMultiElem<ClConstDocumentObjectType>;
+  flags: XmlMultiElem<ClConstDocumentFlag>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  set_value(logId: number, value: null | undefined | string): boolean;
 }
 
-type ClConstDocument = XmlDocument & { TopElem: ClConstDocumentTopElem; };
+type ClConstDocument = XmlDocument & {
+  TopElem: ClConstDocumentTopElem;
+};
