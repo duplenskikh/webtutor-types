f19a4c23c43455d27bcaf06c350b5c0f40bd9e39
umbrik <idumbrik@gmail.com>
2022-08-03 10:13:09 +0300

    ref benefit_item

--- lib/xml/benefit_item/index.d.ts
+++ lib/xml/benefit_item/index.d.ts
@@ -1,18 +1,21 @@
-type BenefitItemDocumentTopElem = XmlTopElem & { Doc: BenefitItemDocument } & 
+type BenefitItemDocumentTopElem = XmlTopElem &
 PersonFillingBase &
 FileListBase &
 CustomElemsBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  status?: XmlElem<string>;
-  start_date?: XmlElem<Date>;
-  finish_date?: XmlElem<Date>;
-  person_id?: XmlElem<number>;
-  benefit_id?: XmlElem<number>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: BenefitItemDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  status: XmlElem<string>;
+  start_date: XmlElem<Date>;
+  finish_date: XmlElem<Date>;
+  person_id: XmlElem<number>;
+  benefit_id: XmlElem<number>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type BenefitItemDocument = XmlDocument & { TopElem: BenefitItemDocumentTopElem; };
+type BenefitItemDocument = XmlDocument & {
+  TopElem: BenefitItemDocumentTopElem;
+};
