5a919e56c5d63718bacfb48298716d5f6a568b8e
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 09:41:55 +0300

    ref application_instance

--- lib/xml/application_instance/index.d.ts
+++ lib/xml/application_instance/index.d.ts
@@ -1,11 +1,14 @@
-type ApplicationInstanceDocumentTopElem = XmlTopElem & { Doc: ApplicationInstanceDocument } & 
+type ApplicationInstanceDocumentTopElem = XmlTopElem &
 WebVariablesBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  status?: XmlElem<string>;
-  application_id?: XmlElem<number>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: ApplicationInstanceDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  status: XmlElem<string>;
+  application_id: XmlElem<number>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type ApplicationInstanceDocument = XmlDocument & { TopElem: ApplicationInstanceDocumentTopElem; };
+type ApplicationInstanceDocument = XmlDocument & {
+  TopElem: ApplicationInstanceDocumentTopElem;
+};
