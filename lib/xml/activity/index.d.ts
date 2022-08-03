0ec8f017b2b99ce99dd73719c5fa8fdf0b0cf018
umbrik <idumbrik@gmail.com>
2022-08-03 09:31:57 +0300

    ref activity

--- lib/xml/activity/index.d.ts
+++ lib/xml/activity/index.d.ts
@@ -1,13 +1,21 @@
-type ActivityDocumentTopElem = XmlTopElem & { Doc: ActivityDocument } & 
-WebVariablesBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  is_eval?: XmlElem<boolean>;
-  statements_eval?: XmlElem<string>;
-  statements_eval_url?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+interface ActivityDocumentRequest {
+  json_str: XmlElem<string>;
 }
 
-type ActivityDocument = XmlDocument & { TopElem: ActivityDocumentTopElem; };
+type ActivityDocumentTopElem = XmlTopElem &
+WebVariablesBase & {
+  Doc: ActivityDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  is_eval: XmlElem<boolean>;
+  statements_eval: XmlElem<string>;
+  statements_eval_url: XmlElem<string>;
+  request: XmlElem<ActivityDocumentRequest>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+}
+
+type ActivityDocument = XmlDocument & {
+  TopElem: ActivityDocumentTopElem;
+};
