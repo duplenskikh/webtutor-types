c9deab35f013acd4ba4ca0240d39483d95cb60a2
umbrik <idumbrik@gmail.com>
2022-08-03 10:29:47 +0300

    ref budget_period

--- lib/xml/budget_period/index.d.ts
+++ lib/xml/budget_period/index.d.ts
@@ -1,22 +1,25 @@
 interface BudgetPeriodDocumentDay {
-  date?: XmlElem<Date>;
-  type?: XmlElem<string>;
-  region_id?: XmlElem<number>;
-  comment?: XmlElem<string>;
+  date: XmlElem<Date>;
+  type: XmlElem<string>;
+  region_id: XmlElem<number>;
+  comment: XmlElem<string>;
 }
 
-type BudgetPeriodDocumentTopElem = XmlTopElem & { Doc: BudgetPeriodDocument } & 
+type BudgetPeriodDocumentTopElem = XmlTopElem &
 AdminAccessBase &
 CustomElemsBase & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  parent_id?: XmlElem<number>;
-  start_date?: XmlElem<Date>;
-  finish_date?: XmlElem<Date>;
-  period_type?: XmlElem<string>;
-  days?: XmlMultiElem<BudgetPeriodDocumentDay>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: BudgetPeriodDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  parent_id: XmlElem<number>;
+  start_date: XmlElem<Date>;
+  finish_date: XmlElem<Date>;
+  period_type: XmlElem<string>;
+  days: XmlMultiElem<BudgetPeriodDocumentDay>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type BudgetPeriodDocument = XmlDocument & { TopElem: BudgetPeriodDocumentTopElem; };
+type BudgetPeriodDocument = XmlDocument & {
+  TopElem: BudgetPeriodDocumentTopElem;
+};
