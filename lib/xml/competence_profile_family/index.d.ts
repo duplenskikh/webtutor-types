3fcfa20cb8740dc708d0f0e094ef2798ae5610b6
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 11:44:13 +0300

    ref competence_profile_family

--- lib/xml/competence_profile_family/index.d.ts
+++ lib/xml/competence_profile_family/index.d.ts
@@ -1,14 +1,17 @@
 interface CompetenceProfileFamilyDocumentCompetenceProfile {
-  competence_profile_id?: XmlElem<number>;
+  competence_profile_id: XmlElem<number>;
 }
 
-type CompetenceProfileFamilyDocumentTopElem = XmlTopElem & { Doc: CompetenceProfileFamilyDocument } & 
+type CompetenceProfileFamilyDocumentTopElem = XmlTopElem &
 AdminAccessBase & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  competence_profiles?: XmlMultiElem<CompetenceProfileFamilyDocumentCompetenceProfile>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: CompetenceProfileFamilyDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  competence_profiles: XmlMultiElem<CompetenceProfileFamilyDocumentCompetenceProfile>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type CompetenceProfileFamilyDocument = XmlDocument & { TopElem: CompetenceProfileFamilyDocumentTopElem; };
+type CompetenceProfileFamilyDocument = XmlDocument & {
+  TopElem: CompetenceProfileFamilyDocumentTopElem;
+};
