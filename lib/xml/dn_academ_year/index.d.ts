31f4460a5d40c6cab6ee137e1e23776d73645de0
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 12:36:33 +0300

    ref dn_academ_year

--- lib/xml/dn_academ_year/index.d.ts
+++ lib/xml/dn_academ_year/index.d.ts
@@ -1,21 +1,24 @@
 interface DnAcademYearDocumentPeriod {
-  id?: XmlElem<string>;
-  start_date?: XmlElem<Date>;
-  finish_date?: XmlElem<Date>;
-  desc?: XmlElem<string>;
+  id: XmlElem<string>;
+  start_date: XmlElem<Date>;
+  finish_date: XmlElem<Date>;
+  desc: XmlElem<string>;
 }
 
-type DnAcademYearDocumentTopElem = XmlTopElem & { Doc: DnAcademYearDocument } & 
+type DnAcademYearDocumentTopElem = XmlTopElem &
 AdminAccessBase &
 CustomElemsBase & {
-  start_year?: XmlElem<string>;
-  end_year?: XmlElem<string>;
-  name?: XmlElem<string>;
-  amount_week_autumnal_term?: XmlElem<number>;
-  amount_week_spring_term?: XmlElem<number>;
-  str_years?: XmlElem<string>;
-  periods?: XmlMultiElem<DnAcademYearDocumentPeriod>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: DnAcademYearDocument;
+  start_year: XmlElem<string>;
+  end_year: XmlElem<string>;
+  name: XmlElem<string>;
+  amount_week_autumnal_term: XmlElem<number>;
+  amount_week_spring_term: XmlElem<number>;
+  str_years: XmlElem<string>;
+  periods: XmlMultiElem<DnAcademYearDocumentPeriod>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type DnAcademYearDocument = XmlDocument & { TopElem: DnAcademYearDocumentTopElem; };
+type DnAcademYearDocument = XmlDocument & {
+  TopElem: DnAcademYearDocumentTopElem;
+};
