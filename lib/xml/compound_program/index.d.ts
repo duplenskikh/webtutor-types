f10cab530d853a1b6a1cc024b6fef7a58e1ac5d4
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 11:50:28 +0300

    ref compound_program

--- lib/xml/compound_program/index.d.ts
+++ lib/xml/compound_program/index.d.ts
@@ -1,69 +1,108 @@
 interface CompoundProgramDocumentProgramStartLearningTask {
-  learning_task_id?: XmlElem<number>;
+  learning_task_id: XmlElem<number>;
 }
 
 interface CompoundProgramDocumentProgramStartAssessment {
-  assessment_id?: XmlElem<number>;
+  assessment_id: XmlElem<number>;
 }
 
 interface CompoundProgramDocumentProgramFinishLearningTask {
-  learning_task_id?: XmlElem<number>;
+  learning_task_id: XmlElem<number>;
 }
 
 interface CompoundProgramDocumentProgramFinishAssessment {
-  assessment_id?: XmlElem<number>;
+  assessment_id: XmlElem<number>;
 }
 
 interface CompoundProgramDocumentProgramCompletedParentProgram {
-  program_id?: XmlElem<number>;
-}
-interface CompoundProgramDocumentProgram extends CostCurrencyTypeBase,
-  CustomElemsBase {
-  id?: XmlElem<number>;
-  name?: XmlElem<string>;
-  parent_progpam_id?: XmlElem<number>;
-  education_program_id?: XmlElem<number>;
-  education_method_id?: XmlElem<number>;
-  start_learning_tasks?: XmlMultiElem<CompoundProgramDocumentProgramStartLearningTask>;
-  start_assessments?: XmlMultiElem<CompoundProgramDocumentProgramStartAssessment>;
-  finish_learning_tasks?: XmlMultiElem<CompoundProgramDocumentProgramFinishLearningTask>;
-  finish_assessments?: XmlMultiElem<CompoundProgramDocumentProgramFinishAssessment>;
-  duration?: XmlElem<number>;
-  person_num?: XmlElem<number>;
-  type?: XmlElem<string>;
-  object_id?: XmlElem<number>;
-  object_name?: XmlElem<string>;
-  object_code?: XmlElem<string>;
-  catalog_name?: XmlElem<string>;
-  subject?: XmlElem<string>;
-  body?: XmlElem<string>;
-  body_type?: XmlElem<string>;
-  edit_notification?: XmlElem<boolean>;
-  delay_days?: XmlElem<number>;
-  days?: XmlElem<number>;
-  weight?: XmlElem<number>;
-  start_type?: XmlElem<string>;
-  required?: XmlElem<boolean>;
-  comment?: XmlElem<string>;
-  completed_parent_programs?: XmlMultiElem<CompoundProgramDocumentProgramCompletedParentProgram>;
+  program_id: XmlElem<number>;
 }
 
-type CompoundProgramDocumentTopElem = XmlTopElem & { Doc: CompoundProgramDocument } & 
+interface CompoundProgramDocumentProgramFinishNotifiation {
+  notification_template_id: XmlElem<number>;
+  subject: XmlElem<string>;
+  body: XmlElem<string>;
+  body_type: XmlElem<string>;
+  edit_notification: XmlElem<boolean>;
+}
+
+interface CompoundProgramDocumentProgramStartNotifiation {
+  notification_template_id: XmlElem<number>;
+  subject: XmlElem<string>;
+  body: XmlElem<string>;
+  body_type: XmlElem<string>;
+  edit_notification: XmlElem<boolean>;
+}
+
+interface CompoundProgramDocumentProgram extends CostCurrencyTypeBase,
+  CustomElemsBase {
+  id: XmlElem<number>;
+  name: XmlElem<string>;
+  parent_progpam_id: XmlElem<number>;
+  education_program_id: XmlElem<number>;
+  education_method_id: XmlElem<number>;
+  start_learning_tasks: XmlMultiElem<CompoundProgramDocumentProgramStartLearningTask>;
+  start_assessments: XmlMultiElem<CompoundProgramDocumentProgramStartAssessment>;
+  finish_learning_tasks: XmlMultiElem<CompoundProgramDocumentProgramFinishLearningTask>;
+  finish_assessments: XmlMultiElem<CompoundProgramDocumentProgramFinishAssessment>;
+  finish_notifiation: XmlElem<CompoundProgramDocumentProgramFinishNotifiation>;
+  start_notifiation: XmlElem<CompoundProgramDocumentProgramStartNotifiation>;
+  duration: XmlElem<number>;
+  person_num: XmlElem<number>;
+  type: XmlElem<string>;
+  object_id: XmlElem<number>;
+  object_name: XmlElem<string>;
+  object_code: XmlElem<string>;
+  catalog_name: XmlElem<string>;
+  subject: XmlElem<string>;
+  body: XmlElem<string>;
+  body_type: XmlElem<string>;
+  edit_notification: XmlElem<boolean>;
+  delay_days: XmlElem<number>;
+  days: XmlElem<number>;
+  weight: XmlElem<number>;
+  start_type: XmlElem<string>;
+  required: XmlElem<boolean>;
+  comment: XmlElem<string>;
+  completed_parent_programs: XmlMultiElem<CompoundProgramDocumentProgramCompletedParentProgram>;
+}
+
+interface IActivateProgramToPersonObject {
+  budget_period_id?: number;
+  collaborators?: number[];
+}
+
+interface IActivateProgramToPersonResultResult {
+  id: number;
+  error: 1;
+  text: string;
+}
+
+interface IActivateProgramToPersonResult {
+  error: number;
+  errorText: string;
+  result: IActivateProgramToPersonResultResult[];
+}
+
+type CompoundProgramDocumentTopElem = XmlTopElem &
 ObjectCodeNameBase &
 LectorsBase &
 FileListBase &
 KnowledgePartsBase &
 CustomElemsBase &
 AdminAccessBase & {
-  desc?: XmlElem<string>;
-  min_person_num?: XmlElem<number>;
-  allow_self_assignment?: XmlElem<boolean>;
-  programs?: XmlMultiElem<CompoundProgramDocumentProgram>;
-  doc_info?: XmlElem<DocInfoBase>;
-  comment?: XmlElem<string>;
-  access?: XmlElem<AccessDocBase>;
-  role_id?: XmlMultiElem<number>;
-  activate_program_to_person?(): unknown;
+  Doc: CompoundProgramDocument;
+  desc: XmlElem<string>;
+  min_person_num: XmlElem<number>;
+  allow_self_assignment: XmlElem<boolean>;
+  programs: XmlMultiElem<CompoundProgramDocumentProgram>;
+  doc_info: XmlElem<DocInfoBase>;
+  comment: XmlElem<string>;
+  access: XmlElem<AccessDocBase>;
+  role_id: XmlMultiElem<number>;
+  activate_program_to_person(object: IActivateProgramToPersonObject): IActivateProgramToPersonResult;
 }
 
-type CompoundProgramDocument = XmlDocument & { TopElem: CompoundProgramDocumentTopElem; };
+type CompoundProgramDocument = XmlDocument & {
+  TopElem: CompoundProgramDocumentTopElem;
+};
