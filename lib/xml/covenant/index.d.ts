168624ef673a16a09fdb544ca418b0f24165179f
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 12:13:04 +0300

    ref covenant

--- lib/xml/covenant/index.d.ts
+++ lib/xml/covenant/index.d.ts
@@ -1,27 +1,30 @@
-type CovenantDocumentTopElem = XmlTopElem & { Doc: CovenantDocument } & 
+type CovenantDocumentTopElem = XmlTopElem &
 CostCurrencyBase &
 FileListBase &
 AdminAccessBase &
 CustomElemsBase & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  signatory_employer_id?: XmlElem<number>;
-  number?: XmlElem<string>;
-  date?: XmlElem<Date>;
-  close_date?: XmlElem<Date>;
-  period_work?: XmlElem<number>;
-  proc_pay_bank?: XmlElem<number>;
-  proc_pay_collab?: XmlElem<number>;
-  sum_pay_bank?(): number;
-  sum_pay_collab?(): number;
-  proc_deduct?: XmlElem<number>;
-  education_method_id?: XmlElem<number>;
-  education_org_id?: XmlElem<number>;
-  event_id?: XmlElem<number>;
-  place?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  status_id?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: CovenantDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  signatory_employer_id: XmlElem<number>;
+  number: XmlElem<string>;
+  date: XmlElem<Date>;
+  close_date: XmlElem<Date>;
+  period_work: XmlElem<number>;
+  proc_pay_bank: XmlElem<number>;
+  proc_pay_collab: XmlElem<number>;
+  sum_pay_bank(): number;
+  sum_pay_collab(): number;
+  proc_deduct: XmlElem<number>;
+  education_method_id: XmlElem<number>;
+  education_org_id: XmlElem<number>;
+  event_id: XmlElem<number>;
+  place: XmlElem<string>;
+  comment: XmlElem<string>;
+  status_id: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type CovenantDocument = XmlDocument & { TopElem: CovenantDocumentTopElem; };
+type CovenantDocument = XmlDocument & {
+  TopElem: CovenantDocumentTopElem;
+};
