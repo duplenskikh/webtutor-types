1e8818a07676ce73c8dd83b307d4be121ff40305
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 12:14:32 +0300

    ref custom_admin_template

--- lib/xml/custom_admin_template/index.d.ts
+++ lib/xml/custom_admin_template/index.d.ts
@@ -1,17 +1,20 @@
-type CustomAdminTemplateDocumentTopElem = XmlTopElem & { Doc: CustomAdminTemplateDocument } & 
+type CustomAdminTemplateDocumentTopElem = XmlTopElem &
 ExecCodeBase &
 WebVariablesBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  category?: XmlElem<string>;
-  form_name?: XmlElem<string>;
-  xms?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  changed?: XmlElem<boolean>;
-  is_std?: XmlElem<boolean>;
-  role_id?: XmlMultiElem<number>;
+  Doc: CustomAdminTemplateDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  category: XmlElem<string>;
+  form_name: XmlElem<string>;
+  xms: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  changed: XmlElem<boolean>;
+  is_std: XmlElem<boolean>;
+  role_id: XmlMultiElem<number>;
 }
 
-type CustomAdminTemplateDocument = XmlDocument & { TopElem: CustomAdminTemplateDocumentTopElem; };
+type CustomAdminTemplateDocument = XmlDocument & {
+  TopElem: CustomAdminTemplateDocumentTopElem;
+};
