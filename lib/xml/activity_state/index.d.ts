ccd126256cdbc654f63a18f21d36e8962152a196
umbrik <idumbrik@gmail.com>
2022-08-03 09:33:45 +0300

    ref activity_state

--- lib/xml/activity_state/index.d.ts
+++ lib/xml/activity_state/index.d.ts
@@ -1,14 +1,25 @@
-type ActivityStateDocumentTopElem = XmlTopElem & { Doc: ActivityStateDocument } & 
-PersonFillingBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  create_date?: XmlElem<Date>;
-  activity_id?: XmlElem<number>;
-  activity_code?: XmlElem<string>;
-  person_id?: XmlElem<number>;
-  registration?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+interface ActivityStateDocumentRequest {
+  content_type: XmlElem<string>;
+  json_str: XmlElem<string>;
+  agent_json_str: XmlElem<string>;
+  agent: XmlElem<ActorBase>;
 }
 
-type ActivityStateDocument = XmlDocument & { TopElem: ActivityStateDocumentTopElem; };
+type ActivityStateDocumentTopElem = XmlTopElem &
+PersonFillingBase & {
+  Doc: ActivityStateDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  create_date: XmlElem<Date>;
+  activity_id: XmlElem<number>;
+  activity_code: XmlElem<string>;
+  person_id: XmlElem<number>;
+  registration: XmlElem<string>;
+  request: XmlElem<ActivityStateDocumentRequest>;
+  doc_info: XmlElem<DocInfoBase>;
+}
+
+type ActivityStateDocument = XmlDocument & {
+  TopElem: ActivityStateDocumentTopElem;
+};
