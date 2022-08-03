cfbb7d38e5aea33664b11a971be48561041e8606
umbrik <idumbrik@gmail.com>
2022-08-03 11:45:12 +0300

    ref component_package

--- lib/xml/component_package/index.d.ts
+++ lib/xml/component_package/index.d.ts
@@ -1,22 +1,25 @@
-type ComponentPackageDocumentTopElem = XmlTopElem & { Doc: ComponentPackageDocument } & 
+type ComponentPackageDocumentTopElem = XmlTopElem &
 FileListBase &
 KnowledgePartsBase &
 CustomElemsBase & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  download_date?: XmlElem<Date>;
-  is_download?: XmlElem<boolean>;
-  is_install?: XmlElem<boolean>;
-  type_id?: XmlElem<string>;
-  component_modification_date?: XmlElem<Date>;
-  is_std?: XmlElem<boolean>;
-  changed?: XmlElem<boolean>;
-  download_package_file_id?: XmlElem<number>;
-  doc_info?: XmlElem<DocInfoBase>;
-  access?: XmlElem<AccessDocBase>;
-  text_area?: XmlElem<string>;
-  load_file?(): unknown;
+  Doc: ComponentPackageDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  comment: XmlElem<string>;
+  download_date: XmlElem<Date>;
+  is_download: XmlElem<boolean>;
+  is_install: XmlElem<boolean>;
+  type_id: XmlElem<string>;
+  component_modification_date: XmlElem<Date>;
+  is_std: XmlElem<boolean>;
+  changed: XmlElem<boolean>;
+  download_package_file_id: XmlElem<number>;
+  doc_info: XmlElem<DocInfoBase>;
+  access: XmlElem<AccessDocBase>;
+  text_area: XmlElem<string>;
+  load_file(): number | void;
 }
 
-type ComponentPackageDocument = XmlDocument & { TopElem: ComponentPackageDocumentTopElem; };
+type ComponentPackageDocument = XmlDocument & {
+  TopElem: ComponentPackageDocumentTopElem;
+};
