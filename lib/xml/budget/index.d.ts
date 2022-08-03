323418937ae48d846bbd9a63a830dd9e767426a3
umbrik <idumbrik@gmail.com>
2022-08-03 10:25:17 +0300

    ref budget

--- lib/xml/budget/index.d.ts
+++ lib/xml/budget/index.d.ts
@@ -1,31 +1,34 @@
 interface BudgetDocumentTransfer {
-  id?: XmlElem<string>;
-  budget_id?: XmlElem<number>;
-  date?: XmlElem<Date>;
-  login?: XmlElem<string>;
-  sum?: XmlElem<number>;
-  direction?: XmlElem<boolean>;
-  comment?: XmlElem<string>;
+  id: XmlElem<string>;
+  budget_id: XmlElem<number>;
+  date: XmlElem<Date>;
+  login: XmlElem<string>;
+  sum: XmlElem<number>;
+  direction: XmlElem<boolean>;
+  comment: XmlElem<string>;
 }
 
-type BudgetDocumentTopElem = XmlTopElem & { Doc: BudgetDocument } & 
+type BudgetDocumentTopElem = XmlTopElem &
 CostCurrencyBase &
 CatalogListBase &
 AdminAccessBase &
 CustomElemsBase & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  state?: XmlElem<string>;
-  type_id?: XmlElem<number>;
-  cost_center_id?: XmlElem<number>;
-  budget_period_id?: XmlElem<number>;
-  expense_item_id?: XmlElem<number>;
-  is_approved?: XmlElem<boolean>;
-  create_date?: XmlElem<Date>;
-  transfers?: XmlMultiElem<BudgetDocumentTransfer>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: BudgetDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  state: XmlElem<string>;
+  type_id: XmlElem<number>;
+  cost_center_id: XmlElem<number>;
+  budget_period_id: XmlElem<number>;
+  expense_item_id: XmlElem<number>;
+  is_approved: XmlElem<boolean>;
+  create_date: XmlElem<Date>;
+  transfers: XmlMultiElem<BudgetDocumentTransfer>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type BudgetDocument = XmlDocument & { TopElem: BudgetDocumentTopElem; };
+type BudgetDocument = XmlDocument & {
+  TopElem: BudgetDocumentTopElem;
+};
