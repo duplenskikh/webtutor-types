64e9f3d0a016f777e47ef22aa4b16ce7a885fe99
umbrik <idumbrik@gmail.com>
2022-08-03 10:10:00 +0300

    ref availability_plan

--- lib/xml/availability_plan/index.d.ts
+++ lib/xml/availability_plan/index.d.ts
@@ -1,26 +1,29 @@
 interface AvailabilityPlanDocumentPeriod {
-  id?: XmlElem<string>;
-  start_date?: XmlElem<Date>;
-  finish_date?: XmlElem<Date>;
-  comment?: XmlElem<string>;
+  id: XmlElem<string>;
+  start_date: XmlElem<Date>;
+  finish_date: XmlElem<Date>;
+  comment: XmlElem<string>;
 }
 
-type AvailabilityPlanDocumentTopElem = XmlTopElem & { Doc: AvailabilityPlanDocument } & 
+type AvailabilityPlanDocumentTopElem = XmlTopElem &
 FileListBase &
 CustomElemsBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  catalog_name?: XmlElem<string>;
-  object_id?: XmlElem<number>;
-  start_date?: XmlElem<Date>;
-  finish_date?: XmlElem<Date>;
-  start_hour?: XmlElem<number>;
-  finish_hour?: XmlElem<number>;
-  periods?: XmlMultiElem<AvailabilityPlanDocumentPeriod>;
-  desc?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  comment?: XmlElem<string>;
+  Doc: AvailabilityPlanDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  catalog_name: XmlElem<string>;
+  object_id: XmlElem<number>;
+  start_date: XmlElem<Date>;
+  finish_date: XmlElem<Date>;
+  start_hour: XmlElem<number>;
+  finish_hour: XmlElem<number>;
+  periods: XmlMultiElem<AvailabilityPlanDocumentPeriod>;
+  desc: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  comment: XmlElem<string>;
 }
 
-type AvailabilityPlanDocument = XmlDocument & { TopElem: AvailabilityPlanDocumentTopElem; };
+type AvailabilityPlanDocument = XmlDocument & {
+  TopElem: AvailabilityPlanDocumentTopElem;
+};
