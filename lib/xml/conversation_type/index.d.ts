d41ae8a19f2575422a5cb80fb6d736a0903aa48f
umbrik <idumbrik@gmail.com>
2022-08-03 12:03:03 +0300

    ref conversation_type

--- lib/xml/conversation_type/index.d.ts
+++ lib/xml/conversation_type/index.d.ts
@@ -1,25 +1,28 @@
 interface ConversationTypeDocumentDispRole {
-  id?: XmlElem<string>;
-  name?: XmlElem<string>;
-  access?: XmlElem<AccessDocBase>;
+  id: XmlElem<string>;
+  name: XmlElem<string>;
+  access: XmlElem<AccessDocBase>;
 }
 
 interface ConversationTypeDocumentCustomState {
-  id?: XmlElem<string>;
-  name?: XmlElem<string>;
+  id: XmlElem<string>;
+  name: XmlElem<string>;
 }
 
-type ConversationTypeDocumentTopElem = XmlTopElem & { Doc: ConversationTypeDocument } & 
+type ConversationTypeDocumentTopElem = XmlTopElem &
 FuncManagersBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  visible_type_id?: XmlElem<string>;
-  disp_roles?: XmlMultiElem<ConversationTypeDocumentDispRole>;
-  custom_states?: XmlMultiElem<ConversationTypeDocumentCustomState>;
-  open_script?: XmlElem<string>;
-  additional_info_script?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: ConversationTypeDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  visible_type_id: XmlElem<string>;
+  disp_roles: XmlMultiElem<ConversationTypeDocumentDispRole>;
+  custom_states: XmlMultiElem<ConversationTypeDocumentCustomState>;
+  open_script: XmlElem<string>;
+  additional_info_script: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type ConversationTypeDocument = XmlDocument & { TopElem: ConversationTypeDocumentTopElem; };
+type ConversationTypeDocument = XmlDocument & {
+  TopElem: ConversationTypeDocumentTopElem;
+};
