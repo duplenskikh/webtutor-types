e6cf3f504f303eef2e6daf7202a657bb4484d176
umbrik <idumbrik@gmail.com>
2022-08-03 11:57:58 +0300

    ref contract_state

--- lib/xml/contract_state/index.d.ts
+++ lib/xml/contract_state/index.d.ts
@@ -1,8 +1,11 @@
-type ContractStateDocumentTopElem = XmlTopElem & { Doc: ContractStateDocument } & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+type ContractStateDocumentTopElem = XmlTopElem & {
+  Doc: ContractStateDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type ContractStateDocument = XmlDocument & { TopElem: ContractStateDocumentTopElem; };
+type ContractStateDocument = XmlDocument & {
+  TopElem: ContractStateDocumentTopElem;
+};
