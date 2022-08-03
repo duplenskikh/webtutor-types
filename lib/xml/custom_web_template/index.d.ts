e3e9d1cd749e35939d335dc4108f6c1f721fa476
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 12:25:33 +0300

    ref custom_web_template

--- lib/xml/custom_web_template/index.d.ts
+++ lib/xml/custom_web_template/index.d.ts
@@ -1,29 +1,37 @@
 interface CustomWebTemplateDocumentIncludeCustomWebTemplate {
-  include_custom_web_template_id?: XmlElem<number>;
+  include_custom_web_template_id: XmlElem<number>;
 }
 
-type CustomWebTemplateDocumentTopElem = XmlTopElem & { Doc: CustomWebTemplateDocument } & 
+interface CustomWebTemplateDocumentTuning {
+  height: XmlElem<number>;
+}
+
+type CustomWebTemplateDocumentTopElem = XmlTopElem &
 ObjectCodeNameBase &
 ExecCodeBase &
 ZonesBase &
 WebVariablesBase & {
-  category?: XmlElem<string>;
-  html?: XmlElem<string>;
-  url?: XmlElem<string>;
-  use_session_cache?: XmlElem<boolean>;
-  cwt_type?: XmlElem<string>;
-  out_type?: XmlElem<string>;
-  mode?: XmlElem<string>;
-  template?: XmlElem<string>;
-  include_custom_web_templates?: XmlMultiElem<CustomWebTemplateDocumentIncludeCustomWebTemplate>;
-  access?: XmlElem<AccessDocBase>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  is_std?: XmlElem<boolean>;
-  changed?: XmlElem<boolean>;
-  is_enabled?: XmlElem<boolean>;
-  role_id?: XmlMultiElem<number>;
+  Doc: CustomWebTemplateDocument;
+  category: XmlElem<string>;
+  html: XmlElem<string>;
+  url: XmlElem<string>;
+  use_session_cache: XmlElem<boolean>;
+  cwt_type: XmlElem<string>;
+  out_type: XmlElem<string>;
+  mode: XmlElem<string>;
+  template: XmlElem<string>;
+  tuning: XmlElem<CustomWebTemplateDocumentTuning>;
+  include_custom_web_templates: XmlMultiElem<CustomWebTemplateDocumentIncludeCustomWebTemplate>;
+  access: XmlElem<AccessDocBase>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  is_std: XmlElem<boolean>;
+  changed: XmlElem<boolean>;
+  is_enabled: XmlElem<boolean>;
+  role_id: XmlMultiElem<number>;
 }
 
-type CustomWebTemplateDocument = XmlDocument & { TopElem: CustomWebTemplateDocumentTopElem; };
+type CustomWebTemplateDocument = XmlDocument & {
+  TopElem: CustomWebTemplateDocumentTopElem;
+};
