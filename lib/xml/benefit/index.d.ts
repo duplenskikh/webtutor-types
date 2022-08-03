f417e8fa9739ea5aa1467b17f752a3484e08e0ef
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 10:12:22 +0300

    ref benefit

--- lib/xml/benefit/index.d.ts
+++ lib/xml/benefit/index.d.ts
@@ -1,14 +1,17 @@
-type BenefitDocumentTopElem = XmlTopElem & { Doc: BenefitDocument } & 
+type BenefitDocumentTopElem = XmlTopElem &
 FileListBase &
 AdminAccessBase &
 CustomElemsBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  weight?: XmlElem<number>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: BenefitDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  weight: XmlElem<number>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type BenefitDocument = XmlDocument & { TopElem: BenefitDocumentTopElem; };
+type BenefitDocument = XmlDocument & {
+  TopElem: BenefitDocumentTopElem;
+};
