dec39adadc68145ad37e356970063e82c5e49688
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 11:57:24 +0300

    ref contract

--- lib/xml/contract/index.d.ts
+++ lib/xml/contract/index.d.ts
@@ -1,22 +1,25 @@
-type ContractDocumentTopElem = XmlTopElem & { Doc: ContractDocument } & 
+type ContractDocumentTopElem = XmlTopElem &
 CostCurrencyBase &
 FileListBase &
 AdminAccessBase &
 CustomElemsBase & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  date?: XmlElem<Date>;
-  number?: XmlElem<string>;
-  contract_type_id?: XmlElem<number>;
-  start_date?: XmlElem<Date>;
-  finish_date?: XmlElem<Date>;
-  education_org_id?: XmlElem<number>;
-  legal_entity_name?: XmlElem<string>;
-  legal_entity_code?: XmlElem<string>;
-  files_count?(): unknown;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: ContractDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  date: XmlElem<Date>;
+  number: XmlElem<string>;
+  contract_type_id: XmlElem<number>;
+  start_date: XmlElem<Date>;
+  finish_date: XmlElem<Date>;
+  education_org_id: XmlElem<number>;
+  legal_entity_name: XmlElem<string>;
+  legal_entity_code: XmlElem<string>;
+  files_count(): number;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type ContractDocument = XmlDocument & { TopElem: ContractDocumentTopElem; };
+type ContractDocument = XmlDocument & {
+  TopElem: ContractDocumentTopElem;
+};
