6740c8b1d346cdf9a708e20283ac9f0678e71246
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 11:53:10 +0300

    ref contact

--- lib/xml/contact/index.d.ts
+++ lib/xml/contact/index.d.ts
@@ -1,29 +1,32 @@
 interface ContactDocumentPollResult {
-  poll_result_id?: XmlElem<number>;
+  poll_result_id: XmlElem<number>;
 }
 
-type ContactDocumentTopElem = XmlTopElem & { Doc: ContactDocument } & 
+type ContactDocumentTopElem = XmlTopElem &
 FileListBase &
 AdminAccessBase &
 CustomElemsBase &
 CustomDatasBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  project_id?: XmlElem<number>;
-  type_id?: XmlElem<number>;
-  initiator_person_fullname?(): unknown;
-  contact_person_fullname?(): unknown;
-  initiator_person_id?: XmlElem<number>;
-  contact_person_id?: XmlElem<number>;
-  contact_org_id?: XmlElem<number>;
-  status_id?: XmlElem<string>;
-  contact_date?: XmlElem<Date>;
-  previous_contact_id?: XmlElem<number>;
-  result_id?: XmlElem<number>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  poll_results?: XmlMultiElem<ContactDocumentPollResult>;
-  desc?: XmlElem<string>;
+  Doc: ContactDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  project_id: XmlElem<number>;
+  type_id: XmlElem<number>;
+  initiator_person_fullname(): XmlElem<string>;
+  contact_person_fullname(): XmlElem<string>;
+  initiator_person_id: XmlElem<number>;
+  contact_person_id: XmlElem<number>;
+  contact_org_id: XmlElem<number>;
+  status_id: XmlElem<string>;
+  contact_date: XmlElem<Date>;
+  previous_contact_id: XmlElem<number>;
+  result_id: XmlElem<number>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  poll_results: XmlMultiElem<ContactDocumentPollResult>;
+  desc: XmlElem<string>;
 }
 
-type ContactDocument = XmlDocument & { TopElem: ContactDocumentTopElem; };
+type ContactDocument = XmlDocument & {
+  TopElem: ContactDocumentTopElem;
+};
