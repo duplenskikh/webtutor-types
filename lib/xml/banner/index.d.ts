d3674a4183e0a201c8cb71d705ab468e51c79302
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 10:10:55 +0300

    ref banner

--- lib/xml/banner/index.d.ts
+++ lib/xml/banner/index.d.ts
@@ -1,19 +1,22 @@
-type BannerDocumentTopElem = XmlTopElem & { Doc: BannerDocument } & 
+type BannerDocumentTopElem = XmlTopElem &
 AdminAccessBase & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  type?: XmlElem<string>;
-  zone?: XmlElem<string>;
-  img_url?: XmlElem<string>;
-  url?: XmlElem<string>;
-  html_text?: XmlElem<string>;
-  start_date?: XmlElem<Date>;
-  finish_date?: XmlElem<Date>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  access?: XmlElem<AccessDocBase>;
-  role_id?: XmlMultiElem<number>;
+  Doc: BannerDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  type: XmlElem<string>;
+  zone: XmlElem<string>;
+  img_url: XmlElem<string>;
+  url: XmlElem<string>;
+  html_text: XmlElem<string>;
+  start_date: XmlElem<Date>;
+  finish_date: XmlElem<Date>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  access: XmlElem<AccessDocBase>;
+  role_id: XmlMultiElem<number>;
 }
 
-type BannerDocument = XmlDocument & { TopElem: BannerDocumentTopElem; };
+type BannerDocument = XmlDocument & {
+  TopElem: BannerDocumentTopElem;
+};
