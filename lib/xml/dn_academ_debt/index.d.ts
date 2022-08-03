45fc835a5028efa146b70274b3cf54247563e6b9
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 12:35:45 +0300

    ref dn_academ_debt

--- lib/xml/dn_academ_debt/index.d.ts
+++ lib/xml/dn_academ_debt/index.d.ts
@@ -1,18 +1,21 @@
-type DnAcademDebtDocumentTopElem = XmlTopElem & { Doc: DnAcademDebtDocument } & 
+type DnAcademDebtDocumentTopElem = XmlTopElem &
 AdminAccessBase &
 CustomElemsBase & {
-  code?: XmlElem<string>;
-  faculty_id?: XmlElem<number>;
-  chair_id?: XmlElem<number>;
-  state_id?: XmlElem<string>;
-  stream_id?: XmlElem<number>;
-  stud_group_id?: XmlElem<number>;
-  student_id?: XmlElem<number>;
-  discipl_id?: XmlElem<number>;
-  contr_form_id?: XmlElem<number>;
-  latest_date?: XmlElem<Date>;
-  liquid_date?: XmlElem<Date>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: DnAcademDebtDocument;
+  code: XmlElem<string>;
+  faculty_id: XmlElem<number>;
+  chair_id: XmlElem<number>;
+  state_id: XmlElem<string>;
+  stream_id: XmlElem<number>;
+  stud_group_id: XmlElem<number>;
+  student_id: XmlElem<number>;
+  discipl_id: XmlElem<number>;
+  contr_form_id: XmlElem<number>;
+  latest_date: XmlElem<Date>;
+  liquid_date: XmlElem<Date>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type DnAcademDebtDocument = XmlDocument & { TopElem: DnAcademDebtDocumentTopElem; };
+type DnAcademDebtDocument = XmlDocument & {
+  TopElem: DnAcademDebtDocumentTopElem;
+};
