4379dd0d66625544ce0417506b3d0bd0d10b18c5
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 12:15:41 +0300

    ref custom_chatbot_template

--- lib/xml/custom_chatbot_template/index.d.ts
+++ lib/xml/custom_chatbot_template/index.d.ts
@@ -1,23 +1,27 @@
 interface CustomChatbotTemplateDocumentIncludeCustomChatbotTemplate {
-  include_custom_chatbot_template_id?: XmlElem<number>;
+  include_custom_chatbot_template_id: XmlElem<number>;
 }
 
-type CustomChatbotTemplateDocumentTopElem = XmlTopElem & { Doc: CustomChatbotTemplateDocument } & 
-ExecCodeBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  eval_str?: XmlElem<string>;
-  url?: XmlElem<string>;
-  out_type?: XmlElem<string>;
-  include_custom_chatbot_templates?: XmlMultiElem<CustomChatbotTemplateDocumentIncludeCustomChatbotTemplate>;
-  access?: XmlElem<AccessDocBase>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  is_std?: XmlElem<boolean>;
-  changed?: XmlElem<boolean>;
-  role_id?: XmlMultiElem<number>;
+type CustomChatbotTemplateDocumentTopElem = XmlTopElem &
+ExecCodeBase &
+MsParametersBase & {
+  Doc: CustomChatbotTemplateDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  eval_str: XmlElem<string>;
+  url: XmlElem<string>;
+  out_type: XmlElem<string>;
+  include_custom_chatbot_templates: XmlMultiElem<CustomChatbotTemplateDocumentIncludeCustomChatbotTemplate>;
+  access: XmlElem<AccessDocBase>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  is_std: XmlElem<boolean>;
+  changed: XmlElem<boolean>;
+  role_id: XmlMultiElem<number>;
 }
 
-type CustomChatbotTemplateDocument = XmlDocument & { TopElem: CustomChatbotTemplateDocumentTopElem; };
+type CustomChatbotTemplateDocument = XmlDocument & {
+  TopElem: CustomChatbotTemplateDocumentTopElem;
+};
