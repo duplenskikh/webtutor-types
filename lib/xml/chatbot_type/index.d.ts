13c80257bcd9a4894191b3d47833e992d285b22b
umbrik <idumbrik@gmail.com>
2022-08-03 10:52:46 +0300

    ref chatbot_type

--- lib/xml/chatbot_type/index.d.ts
+++ lib/xml/chatbot_type/index.d.ts
@@ -1,14 +1,18 @@
-type ChatbotTypeDocumentTopElem = XmlTopElem & { Doc: ChatbotTypeDocument } & 
+type ChatbotTypeDocumentTopElem = XmlTopElem &
+MsParametersBase &
 AdminAccessBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  script_url?: XmlElem<string>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  is_std?: XmlElem<boolean>;
-  changed?: XmlElem<boolean>;
+  Doc: ChatbotTypeDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  script_url: XmlElem<string>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  is_std: XmlElem<boolean>;
+  changed: XmlElem<boolean>;
 }
 
-type ChatbotTypeDocument = XmlDocument & { TopElem: ChatbotTypeDocumentTopElem; };
+type ChatbotTypeDocument = XmlDocument & {
+  TopElem: ChatbotTypeDocumentTopElem;
+};
