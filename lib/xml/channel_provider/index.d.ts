08d2fbb9439bf65ba2a01afc8aa148adf17c6d5b
umbrik <idumbrik@gmail.com>
2022-08-03 10:45:38 +0300

    ref channel_provider

--- lib/xml/channel_provider/index.d.ts
+++ lib/xml/channel_provider/index.d.ts
@@ -1,17 +1,20 @@
-type ChannelProviderDocumentTopElem = XmlTopElem & { Doc: ChannelProviderDocument } & 
+type ChannelProviderDocumentTopElem = XmlTopElem &
 AdminAccessBase &
 WebVariablesBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  code_library_id?: XmlElem<number>;
-  root_function_name?: XmlElem<string>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  is_std?: XmlElem<boolean>;
-  changed?: XmlElem<boolean>;
-  doc_info?: XmlElem<DocInfoBase>;
-  provider_do?(): unknown;
+  Doc: ChannelProviderDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  code_library_id: XmlElem<number>;
+  root_function_name: XmlElem<string>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  is_std: XmlElem<boolean>;
+  changed: XmlElem<boolean>;
+  doc_info: XmlElem<DocInfoBase>;
+  provider_do(): unknown;
 }
 
-type ChannelProviderDocument = XmlDocument & { TopElem: ChannelProviderDocumentTopElem; };
+type ChannelProviderDocument = XmlDocument & {
+  TopElem: ChannelProviderDocumentTopElem;
+};
