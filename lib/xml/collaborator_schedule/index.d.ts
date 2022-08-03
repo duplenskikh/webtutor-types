de70ac76dec0629791a3ce8b7cf0d53a1a2c13f9
umbrik <idumbrik@gmail.com>
2022-08-03 11:36:10 +0300

    ref collaborator_schedule

--- lib/xml/collaborator_schedule/index.d.ts
+++ lib/xml/collaborator_schedule/index.d.ts
@@ -1,35 +1,37 @@
 interface CollaboratorScheduleDocumentPeriod extends ObjectTypeBase,
   CustomElemsBase {
-  id?: XmlElem<string>;
-  presence_state_id?: XmlElem<number>;
-  subdivision_id?: XmlElem<number>;
-  state_id?: XmlElem<string>;
-  start_time?: XmlElem<string>;
-  finish_time?: XmlElem<string>;
-  comment?: XmlElem<string>;
+  id: XmlElem<string>;
+  presence_state_id: XmlElem<number>;
+  subdivision_id: XmlElem<number>;
+  state_id: XmlElem<string>;
+  start_time: XmlElem<string>;
+  finish_time: XmlElem<string>;
+  comment: XmlElem<string>;
 }
 
 type CollaboratorScheduleDocumentTopElem = XmlTopElem & { Doc: CollaboratorScheduleDocument } & 
 PersonFillingBase &
 CustomElemsBase & {
-  id?: XmlElem<number>;
-  person_id?: XmlElem<number>;
-  date?: XmlElem<Date>;
-  start_time?: XmlElem<string>;
-  finish_time?: XmlElem<string>;
-  presence_state_id?: XmlElem<number>;
-  schedule_type_id?: XmlElem<number>;
-  state_id?: XmlElem<string>;
-  approval_date?: XmlElem<Date>;
-  approval_person_id?: XmlElem<number>;
-  budget_period_id?: XmlElem<number>;
-  rest_collaborator_schedule_id?: XmlElem<number>;
-  interval_schedule_id?: XmlElem<number>;
-  schedule_day_id?: XmlElem<number>;
-  periods?: XmlMultiElem<CollaboratorScheduleDocumentPeriod>;
-  reason_comment?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  id: XmlElem<number>;
+  person_id: XmlElem<number>;
+  date: XmlElem<Date>;
+  start_time: XmlElem<string>;
+  finish_time: XmlElem<string>;
+  presence_state_id: XmlElem<number>;
+  schedule_type_id: XmlElem<number>;
+  state_id: XmlElem<string>;
+  approval_date: XmlElem<Date>;
+  approval_person_id: XmlElem<number>;
+  budget_period_id: XmlElem<number>;
+  rest_collaborator_schedule_id: XmlElem<number>;
+  interval_schedule_id: XmlElem<number>;
+  schedule_day_id: XmlElem<number>;
+  periods: XmlMultiElem<CollaboratorScheduleDocumentPeriod>;
+  reason_comment: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type CollaboratorScheduleDocument = XmlDocument & { TopElem: CollaboratorScheduleDocumentTopElem; };
+type CollaboratorScheduleDocument = XmlDocument & {
+  TopElem: CollaboratorScheduleDocumentTopElem;
+};
