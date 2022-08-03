94c7aed23a283eb0cd0b065a2abe3a6c0a4da81d
umbrik <idumbrik@gmail.com>
2022-08-03 09:36:10 +0300

    ref admin_configuration

--- lib/xml/admin_configuration/index.d.ts
+++ lib/xml/admin_configuration/index.d.ts
@@ -1,35 +1,40 @@
 interface AdminConfigurationDocumentFuncBlockSectionApplication {
-  application_id?: XmlElem<number>;
-  name?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  resource_id?: XmlElem<string>;
+  application_id: XmlElem<number>;
+  name: XmlElem<string>;
+  comment: XmlElem<string>;
+  resource_id: XmlElem<string>;
 }
+
 interface AdminConfigurationDocumentFuncBlockSection {
-  id?: XmlElem<string>;
-  name?: XmlElem<string>;
-  color?: XmlElem<string>;
-  applications?: XmlMultiElem<AdminConfigurationDocumentFuncBlockSectionApplication>;
+  id: XmlElem<string>;
+  name: XmlElem<string>;
+  color: XmlElem<string>;
+  applications: XmlMultiElem<AdminConfigurationDocumentFuncBlockSectionApplication>;
 }
+
 interface AdminConfigurationDocumentFuncBlock {
-  id?: XmlElem<string>;
-  name?: XmlElem<string>;
-  resource_id?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  min_height?: XmlElem<number>;
-  width?: XmlElem<number>;
-  sections?: XmlMultiElem<AdminConfigurationDocumentFuncBlockSection>;
+  id: XmlElem<string>;
+  name: XmlElem<string>;
+  resource_id: XmlElem<string>;
+  comment: XmlElem<string>;
+  min_height: XmlElem<number>;
+  width: XmlElem<number>;
+  sections: XmlMultiElem<AdminConfigurationDocumentFuncBlockSection>;
 }
 
-type AdminConfigurationDocumentTopElem = XmlTopElem & { Doc: AdminConfigurationDocument } & 
+type AdminConfigurationDocumentTopElem = XmlTopElem &
 ObjectCodeNameBase & {
-  func_blocks?: XmlMultiElem<AdminConfigurationDocumentFuncBlock>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  access?: XmlElem<AccessDocBase>;
-  desc?: XmlElem<string>;
-  changed?: XmlElem<boolean>;
-  is_std?: XmlElem<boolean>;
-  role_id?: XmlMultiElem<number>;
+  Doc: AdminConfigurationDocument;
+  func_blocks: XmlMultiElem<AdminConfigurationDocumentFuncBlock>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  access: XmlElem<AccessDocBase>;
+  desc: XmlElem<string>;
+  changed: XmlElem<boolean>;
+  is_std: XmlElem<boolean>;
+  role_id: XmlMultiElem<number>;
 }
 
-type AdminConfigurationDocument = XmlDocument & { TopElem: AdminConfigurationDocumentTopElem; };
+type AdminConfigurationDocument = XmlDocument & {
+  TopElem: AdminConfigurationDocumentTopElem;
+};
