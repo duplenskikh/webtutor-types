de375461b5f185cd8cb1c919195e8bba0d447c29
umbrik <idumbrik@gmail.com>
2022-08-03 10:17:07 +0300

    ref blog_entry

--- lib/xml/blog_entry/index.d.ts
+++ lib/xml/blog_entry/index.d.ts
@@ -1,4 +1,4 @@
-type BlogEntryDocumentTopElem = XmlTopElem & { Doc: BlogEntryDocument } & 
+type BlogEntryDocumentTopElem = XmlTopElem &
 PersonFillingBase &
 FileListBase &
 KnowledgePartsBase &
@@ -6,24 +6,27 @@ KnowledgePartsBaseOld &
 AdminAccessBase &
 GameBonusBase &
 CustomElemsBase & {
-  id?: XmlElem<number>;
-  name?: XmlElem<string>;
-  type?: XmlElem<string>;
-  create_date?: XmlElem<Date>;
-  date_modified?: XmlElem<Date>;
-  allow_comment?: XmlElem<boolean>;
-  blocked?: XmlElem<boolean>;
-  text_area?: XmlElem<string>;
-  labels?: XmlElem<string>;
-  person_id?: XmlElem<number>;
-  blog_id?: XmlElem<number>;
-  feed_item_id?: XmlElem<string>;
-  object_id?: XmlElem<number>;
-  object_type?: XmlElem<string>;
-  object_name?: XmlElem<string>;
-  comment_num?: XmlElem<number>;
-  doc_info?: XmlElem<DocInfoBase>;
-  access?: XmlElem<AccessDocBase>;
+  Doc: BlogEntryDocument;
+  id: XmlElem<number>;
+  name: XmlElem<string>;
+  type: XmlElem<string>;
+  create_date: XmlElem<Date>;
+  date_modified: XmlElem<Date>;
+  allow_comment: XmlElem<boolean>;
+  blocked: XmlElem<boolean>;
+  text_area: XmlElem<string>;
+  labels: XmlElem<string>;
+  person_id: XmlElem<number>;
+  blog_id: XmlElem<number>;
+  feed_item_id: XmlElem<string>;
+  object_id: XmlElem<number>;
+  object_type: XmlElem<string>;
+  object_name: XmlElem<string>;
+  comment_num: XmlElem<number>;
+  doc_info: XmlElem<DocInfoBase>;
+  access: XmlElem<AccessDocBase>;
 }
 
-type BlogEntryDocument = XmlDocument & { TopElem: BlogEntryDocumentTopElem; };
+type BlogEntryDocument = XmlDocument & {
+  TopElem: BlogEntryDocumentTopElem;
+};
