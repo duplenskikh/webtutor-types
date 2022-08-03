bc73527a0c36267a3c7c6cabdd3c6883c723fcf1
umbrik <idumbrik@gmail.com>
2022-08-03 11:39:39 +0300

    ref competence_block

--- lib/xml/competence_block/index.d.ts
+++ lib/xml/competence_block/index.d.ts
@@ -1,10 +1,13 @@
-type CompetenceBlockDocumentTopElem = XmlTopElem & { Doc: CompetenceBlockDocument } & 
+type CompetenceBlockDocumentTopElem = XmlTopElem &
 AdminAccessBase & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  parent_object_id?: XmlElem<number>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: CompetenceBlockDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  parent_object_id: XmlElem<number>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type CompetenceBlockDocument = XmlDocument & { TopElem: CompetenceBlockDocumentTopElem; };
+type CompetenceBlockDocument = XmlDocument & {
+  TopElem: CompetenceBlockDocumentTopElem;
+};
