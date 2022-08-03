5a737b72e58227fb8c4efb9e9b21c84760531d7a
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 10:49:40 +0300

    ref chatbot_chat

--- lib/xml/chatbot_chat/index.d.ts
+++ lib/xml/chatbot_chat/index.d.ts
@@ -1,43 +1,46 @@
 interface ChatbotChatDocumentMessage {
-  id?: XmlElem<string>;
-  date?: XmlElem<Date>;
-  text?: XmlElem<string>;
-  last_chatbot_stage_id?: XmlElem<number>;
-  next_chatbot_stage_id?: XmlElem<number>;
+  id: XmlElem<string>;
+  date: XmlElem<Date>;
+  text: XmlElem<string>;
+  last_chatbot_stage_id: XmlElem<number>;
+  next_chatbot_stage_id: XmlElem<number>;
 }
 
 interface ChatbotChatDocumentTempValue {
-  id?: XmlElem<string>;
-  value?: XmlElem<string>;
+  id: XmlElem<string>;
+  value: XmlElem<string>;
 }
 
 interface ChatbotChatDocumentAimlData {
-  custom_chatbot_template_id?: XmlElem<number>;
-  data?: XmlElem<string>;
+  custom_chatbot_template_id: XmlElem<number>;
+  data: XmlElem<string>;
 }
 
-type ChatbotChatDocumentTopElem = XmlTopElem & { Doc: ChatbotChatDocument } & 
+type ChatbotChatDocumentTopElem = XmlTopElem &
 PersonFillingBase &
 CustomElemsBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  resource_id?: XmlElem<number>;
-  bot_id?: XmlElem<string>;
-  object_type?: XmlElem<string>;
-  object_id?: XmlElem<number>;
-  chatbot_stage_id?: XmlElem<number>;
-  chatbot_type_id?: XmlElem<number>;
-  chatbot_id?: XmlElem<number>;
-  conversation_id?: XmlElem<number>;
-  state_id?: XmlElem<string>;
-  lng_id?: XmlElem<string>;
-  person_id?: XmlElem<number>;
-  messages?: XmlMultiElem<ChatbotChatDocumentMessage>;
-  temp_values?: XmlMultiElem<ChatbotChatDocumentTempValue>;
-  aiml_datas?: XmlMultiElem<ChatbotChatDocumentAimlData>;
-  last_message_id?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: ChatbotChatDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  resource_id: XmlElem<number>;
+  bot_id: XmlElem<string>;
+  object_type: XmlElem<string>;
+  object_id: XmlElem<number>;
+  chatbot_stage_id: XmlElem<number>;
+  chatbot_type_id: XmlElem<number>;
+  chatbot_id: XmlElem<number>;
+  conversation_id: XmlElem<number>;
+  state_id: XmlElem<string>;
+  lng_id: XmlElem<string>;
+  person_id: XmlElem<number>;
+  messages: XmlMultiElem<ChatbotChatDocumentMessage>;
+  temp_values: XmlMultiElem<ChatbotChatDocumentTempValue>;
+  aiml_datas: XmlMultiElem<ChatbotChatDocumentAimlData>;
+  last_message_id: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type ChatbotChatDocument = XmlDocument & { TopElem: ChatbotChatDocumentTopElem; };
+type ChatbotChatDocument = XmlDocument & {
+  TopElem: ChatbotChatDocumentTopElem;
+};
