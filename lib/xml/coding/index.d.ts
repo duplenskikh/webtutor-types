4c4d247ffe94b39a50f812bb4fa6aac9110b540e
umbrik <idumbrik@gmail.com>
2022-08-03 11:20:05 +0300

    ref coding

--- lib/xml/coding/index.d.ts
+++ lib/xml/coding/index.d.ts
@@ -1,16 +1,19 @@
-type CodingDocumentTopElem = XmlTopElem & { Doc: CodingDocument } & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  catalog_name?: XmlElem<string>;
-  prefix?: XmlElem<string>;
-  next_index?: XmlElem<number>;
-  digits_num?: XmlElem<number>;
-  field_name?: XmlElem<string>;
-  is_std?: XmlElem<boolean>;
-  changed?: XmlElem<boolean>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  obtain_code?(): unknown;
+type CodingDocumentTopElem = XmlTopElem & {
+  Doc: CodingDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  catalog_name: XmlElem<string>;
+  prefix: XmlElem<string>;
+  next_index: XmlElem<number>;
+  digits_num: XmlElem<number>;
+  field_name: XmlElem<string>;
+  is_std: XmlElem<boolean>;
+  changed: XmlElem<boolean>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  obtain_code(): string;
 }
 
-type CodingDocument = XmlDocument & { TopElem: CodingDocumentTopElem; };
+type CodingDocument = XmlDocument & {
+  TopElem: CodingDocumentTopElem;
+};
