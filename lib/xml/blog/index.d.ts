acda35f4c78491247d0990ff26408a0662fbf845
umbrik <idumbrik@gmail.com>
2022-08-03 10:16:12 +0300

    ref blog

--- lib/xml/blog/index.d.ts
+++ lib/xml/blog/index.d.ts
@@ -1,32 +1,35 @@
 interface BlogDocumentAuthor extends PersonFillingBase {
-  person_id?: XmlElem<number>;
-  is_full_moderator?: XmlElem<boolean>;
+  person_id: XmlElem<number>;
+  is_full_moderator: XmlElem<boolean>;
 }
 
-type BlogDocumentTopElem = XmlTopElem & { Doc: BlogDocument } & 
+type BlogDocumentTopElem = XmlTopElem &
 ObjectCodeNameBase &
 WebVariablesBase &
 PersonFillingBase &
 KnowledgePartsBase &
 AdminAccessBase &
 CustomElemsBase & {
-  create_date?: XmlElem<Date>;
-  type?: XmlElem<string>;
-  channel_provider_id?: XmlElem<number>;
-  permit_subscription?: XmlElem<boolean>;
-  allow_anonymous_comment?: XmlElem<boolean>;
-  creator_id?: XmlElem<number>;
-  object_id?: XmlElem<number>;
-  object_type?: XmlElem<string>;
-  object_name?: XmlElem<string>;
-  authors?: XmlMultiElem<BlogDocumentAuthor>;
-  authors_num?(): unknown;
-  num_message_in_list?: XmlElem<number>;
-  access?: XmlElem<AccessDocBase>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  creator_full_info?(): unknown;
+  Doc: BlogDocument;
+  create_date: XmlElem<Date>;
+  type: XmlElem<string>;
+  channel_provider_id: XmlElem<number>;
+  permit_subscription: XmlElem<boolean>;
+  allow_anonymous_comment: XmlElem<boolean>;
+  creator_id: XmlElem<number>;
+  object_id: XmlElem<number>;
+  object_type: XmlElem<string>;
+  object_name: XmlElem<string>;
+  authors: XmlMultiElem<BlogDocumentAuthor>;
+  authors_num(): number;
+  num_message_in_list: XmlElem<number>;
+  access: XmlElem<AccessDocBase>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  creator_full_info(): string;
 }
 
-type BlogDocument = XmlDocument & { TopElem: BlogDocumentTopElem; };
+type BlogDocument = XmlDocument & {
+  TopElem: BlogDocumentTopElem;
+};
