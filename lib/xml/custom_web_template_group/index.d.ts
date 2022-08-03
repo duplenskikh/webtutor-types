a3bc58a9d3bd9753c6fdcba3cb68a57e51b75ce6
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 12:26:37 +0300

    ref custom_web_template_group

--- lib/xml/custom_web_template_group/index.d.ts
+++ lib/xml/custom_web_template_group/index.d.ts
@@ -1,21 +1,24 @@
 interface CustomWebTemplateGroupDocumentTemplate extends WebVariablesBase {
-  id?: XmlElem<string>;
-  name?: XmlElem<string>;
-  custom_web_template_id?: XmlElem<number>;
-  statistic_rec_id?: XmlElem<number>;
-  resource_id?: XmlElem<number>;
-  parent_template_id?: XmlElem<string>;
-  position?: XmlElem<number>;
-  comment?: XmlElem<string>;
+  id: XmlElem<string>;
+  name: XmlElem<string>;
+  custom_web_template_id: XmlElem<number>;
+  statistic_rec_id: XmlElem<number>;
+  resource_id: XmlElem<number>;
+  parent_template_id: XmlElem<string>;
+  position: XmlElem<number>;
+  comment: XmlElem<string>;
 }
 
-type CustomWebTemplateGroupDocumentTopElem = XmlTopElem & { Doc: CustomWebTemplateGroupDocument } & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  templates?: XmlMultiElem<CustomWebTemplateGroupDocumentTemplate>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  role_id?: XmlMultiElem<number>;
+type CustomWebTemplateGroupDocumentTopElem = XmlTopElem & {
+  Doc: CustomWebTemplateGroupDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  templates: XmlMultiElem<CustomWebTemplateGroupDocumentTemplate>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  role_id: XmlMultiElem<number>;
 }
 
-type CustomWebTemplateGroupDocument = XmlDocument & { TopElem: CustomWebTemplateGroupDocumentTopElem; };
+type CustomWebTemplateGroupDocument = XmlDocument & {
+  TopElem: CustomWebTemplateGroupDocumentTopElem;
+};
