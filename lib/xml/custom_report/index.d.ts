b0a92bd32bf1ab9884af36aab963f7e7fd4147b2
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 12:23:57 +0300

    ref custom_report

--- lib/xml/custom_report/index.d.ts
+++ lib/xml/custom_report/index.d.ts
@@ -1,23 +1,47 @@
-type CustomReportDocumentTopElem = XmlTopElem & { Doc: CustomReportDocument } & 
-CustomReportBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  block?: XmlElem<string>;
-  connect_2_object?: XmlElem<string>;
-  mode_selector?: XmlElem<number>;
-  source_binding_object?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  access?: XmlElem<AccessDocBase>;
-  binding_selection?: XmlElem<number>;
-  initiator_person_id?: XmlElem<number>;
-  perfomance_cutoff?: XmlElem<number>;
-  build_report_v2?(): unknown;
-  buildCustomReportQuery?(): unknown;
-  UI_CoreModeChangeHandler?(): unknown;
-  UI_CoreObjectChangeHandler?(): unknown;
-  SetModeSelector?(): unknown;
+interface CustomReportDocumentWebSettings {
+  table_height: XmlElem<number>;
+  default_mode: XmlElem<string>;
 }
 
-type CustomReportDocument = XmlDocument & { TopElem: CustomReportDocumentTopElem; };
+interface CustomReportDocumentSubscribe {
+  on: XmlElem<boolean>;
+  days_period: XmlElem<number>;
+  format: XmlElem<string>;
+}
+
+interface IBuildReportV2Result {
+  error: number;
+  error_text: string;
+  i: number;
+  count: number;
+}
+
+type CustomReportDocumentTopElem = XmlTopElem &
+CustomReportBase & {
+  Doc: CustomReportDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  block: XmlElem<string>;
+  connect_2_object: XmlElem<string>;
+  mode_selector: XmlElem<number>;
+  source_binding_object: XmlElem<string>;
+  web_settings: XmlElem<CustomReportDocumentWebSettings>;
+  subscribe: XmlElem<CustomReportDocumentSubscribe>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  disp_block: XmlElem<MsDispBlockBase>;
+  access: XmlElem<AccessDocBase>;
+  binding_selection: XmlElem<number>;
+  initiator_person_id: XmlElem<number>;
+  perfomance_cutoff: XmlElem<number>;
+  build_report_v2(taskId?: string, meta?: boolean): IBuildReportV2Result;
+  buildCustomReportQuery(topElem: XmlTopElem, taskId: string): unknown;
+  UI_CoreModeChangeHandler(value: number): void;
+  UI_CoreObjectChangeHandler(fieldSource: XmlElem<unknown>, value: unknown): boolean;
+  SetModeSelector(mode: number): void;
+}
+
+type CustomReportDocument = XmlDocument & {
+  TopElem: CustomReportDocumentTopElem;
+};
