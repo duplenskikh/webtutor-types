9d087aaadee117e4b8a6632d48c306bd35d16988
umbrik <idumbrik@gmail.com>
2022-08-03 12:31:21 +0300

    ref digital_signature

--- lib/xml/digital_signature/index.d.ts
+++ lib/xml/digital_signature/index.d.ts
@@ -1,23 +1,26 @@
-type DigitalSignatureDocumentTopElem = XmlTopElem & { Doc: DigitalSignatureDocument } & 
+type DigitalSignatureDocumentTopElem = XmlTopElem &
 PersonFillingBase &
 FileListBase &
 AdminAccessBase &
 CustomElemsBase & {
-  name?(): string;
-  person_id?: XmlElem<number>;
-  object_type?: XmlElem<string>;
-  object_id?: XmlElem<number>;
-  object_name?: XmlElem<string>;
-  create_date?: XmlElem<Date>;
-  sign_date?: XmlElem<Date>;
-  is_signed?: XmlElem<boolean>;
-  custom_web_template_id?: XmlElem<number>;
-  sign_open?: XmlElem<string>;
-  sign_encrypted?: XmlElem<string>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  access?: XmlElem<AccessDocBase>;
+  Doc: DigitalSignatureDocument;
+  name(): string;
+  person_id: XmlElem<number>;
+  object_type: XmlElem<string>;
+  object_id: XmlElem<number>;
+  object_name: XmlElem<string>;
+  create_date: XmlElem<Date>;
+  sign_date: XmlElem<Date>;
+  is_signed: XmlElem<boolean>;
+  custom_web_template_id: XmlElem<number>;
+  sign_open: XmlElem<string>;
+  sign_encrypted: XmlElem<string>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  access: XmlElem<AccessDocBase>;
 }
 
-type DigitalSignatureDocument = XmlDocument & { TopElem: DigitalSignatureDocumentTopElem; };
+type DigitalSignatureDocument = XmlDocument & {
+  TopElem: DigitalSignatureDocumentTopElem;
+};
