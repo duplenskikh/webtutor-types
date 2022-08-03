75d3746fafeaf9afed06d69bf19ae21fb1c0dc5a
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 09:44:03 +0300

    ref article

--- lib/xml/article/index.d.ts
+++ lib/xml/article/index.d.ts
@@ -1,20 +1,23 @@
-type ArticleDocumentTopElem = XmlTopElem & { Doc: ArticleDocument } & 
+type ArticleDocumentTopElem = XmlTopElem &
 CatalogListBase &
 FileListBase &
 KnowledgePartsBase &
 KnowledgePartsBaseOld &
 AdminAccessBase &
 CustomElemsBase & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  parent_object_id?: XmlElem<number>;
-  book_id?: XmlElem<number>;
-  library_material_id?: XmlElem<number>;
-  weight?: XmlElem<number>;
-  comment?: XmlElem<string>;
-  desc?: XmlElem<string>;
-  access?: XmlElem<AccessDocBase>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: ArticleDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  parent_object_id: XmlElem<number>;
+  book_id: XmlElem<number>;
+  library_material_id: XmlElem<number>;
+  weight: XmlElem<number>;
+  comment: XmlElem<string>;
+  desc: XmlElem<string>;
+  access: XmlElem<AccessDocBase>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type ArticleDocument = XmlDocument & { TopElem: ArticleDocumentTopElem; };
+type ArticleDocument = XmlDocument & {
+  TopElem: ArticleDocumentTopElem;
+};
