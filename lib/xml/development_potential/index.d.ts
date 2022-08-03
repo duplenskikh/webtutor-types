2728cf5ed6a68eb06ecb3883e5d4b843cb600965
umbrik <idumbrik@gmail.com>
2022-08-03 12:29:41 +0300

    ref development_potential

--- lib/xml/development_potential/index.d.ts
+++ lib/xml/development_potential/index.d.ts
@@ -1,15 +1,18 @@
-type DevelopmentPotentialDocumentTopElem = XmlTopElem & { Doc: DevelopmentPotentialDocument } & 
+type DevelopmentPotentialDocumentTopElem = XmlTopElem &
 FileListBase &
 AdminAccessBase &
 CustomElemsBase & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  access?: XmlElem<AccessDocBase>;
-  is_std?: XmlElem<boolean>;
-  changed?: XmlElem<boolean>;
+  Doc: DevelopmentPotentialDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  access: XmlElem<AccessDocBase>;
+  is_std: XmlElem<boolean>;
+  changed: XmlElem<boolean>;
 }
 
-type DevelopmentPotentialDocument = XmlDocument & { TopElem: DevelopmentPotentialDocumentTopElem; };
+type DevelopmentPotentialDocument = XmlDocument & {
+  TopElem: DevelopmentPotentialDocumentTopElem;
+};
