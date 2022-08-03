6650812d81cf394965748f6822e2225df4ce36a2
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 11:37:50 +0300

    ref comittee_member

--- lib/xml/committee_member/index.d.ts
+++ lib/xml/committee_member/index.d.ts
@@ -1,20 +1,23 @@
-type CommitteeMemberDocumentTopElem = XmlTopElem & { Doc: CommitteeMemberDocument } & 
+type CommitteeMemberDocumentTopElem = XmlTopElem &
 FileListBase &
 AdminAccessBase &
 CustomElemsBase & {
-  id?: XmlElem<number>;
-  code?: XmlElem<string>;
-  catalog?: XmlElem<string>;
-  object_id?: XmlElem<number>;
-  object_name?: XmlElem<string>;
-  boss_type_id?: XmlElem<number>;
-  committee_member_type?: XmlElem<string>;
-  status?: XmlElem<string>;
-  personnel_committee_id?: XmlElem<number>;
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  access?: XmlElem<AccessDocBase>;
+  Doc: CommitteeMemberDocument;
+  id: XmlElem<number>;
+  code: XmlElem<string>;
+  catalog: XmlElem<string>;
+  object_id: XmlElem<number>;
+  object_name: XmlElem<string>;
+  boss_type_id: XmlElem<number>;
+  committee_member_type: XmlElem<string>;
+  status: XmlElem<string>;
+  personnel_committee_id: XmlElem<number>;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  access: XmlElem<AccessDocBase>;
 }
 
-type CommitteeMemberDocument = XmlDocument & { TopElem: CommitteeMemberDocumentTopElem; };
+type CommitteeMemberDocument = XmlDocument & {
+  TopElem: CommitteeMemberDocumentTopElem;
+};
