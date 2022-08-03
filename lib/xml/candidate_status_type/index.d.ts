62e3911bd6e3184db792a9e659b7696f1d65eb68
umbrik <693254+duplenskikh@users.noreply.github.com>
2022-08-03 10:32:56 +0300

    ref candidate_status_type

--- lib/xml/candidate_status_type/index.d.ts
+++ lib/xml/candidate_status_type/index.d.ts
@@ -1,12 +1,15 @@
-type CandidateStatusTypeDocumentTopElem = XmlTopElem & { Doc: CandidateStatusTypeDocument } & 
+type CandidateStatusTypeDocumentTopElem = XmlTopElem &
 ObjectCodeNameBase &
 FileListBase &
 AdminAccessBase &
 CustomElemsBase & {
-  desc?: XmlElem<string>;
-  comment?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
-  access?: XmlElem<AccessDocBase>;
+  Doc: CandidateStatusTypeDocument;
+  desc: XmlElem<string>;
+  comment: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+  access: XmlElem<AccessDocBase>;
 }
 
-type CandidateStatusTypeDocument = XmlDocument & { TopElem: CandidateStatusTypeDocumentTopElem; };
+type CandidateStatusTypeDocument = XmlDocument & {
+  TopElem: CandidateStatusTypeDocumentTopElem;
+};
