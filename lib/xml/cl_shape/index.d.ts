6971727ae6ac4dfed42bda09b473689bd2c09c1c
umbrik <idumbrik@gmail.com>
2022-08-03 11:11:05 +0300

    ref cl_shape

--- lib/xml/cl_shape/index.d.ts
+++ lib/xml/cl_shape/index.d.ts
@@ -1,28 +1,41 @@
 interface ClShapeDocumentFormula {
-  expr?: XmlElem<string>;
+  expr: XmlElem<string>;
 }
 
 interface ClShapeDocumentHandle {
-  position?: XmlElem<string>;
-  xrange?: XmlElem<string>;
-  yrange?: XmlElem<string>;
+  position: XmlElem<string>;
+  xrange: XmlElem<string>;
+  yrange: XmlElem<string>;
 }
 
-type ClShapeDocumentTopElem = XmlTopElem & { Doc: ClShapeDocument } & {
-  code?: XmlElem<string>;
-  name?: XmlElem<string>;
-  category?: XmlElem<string>;
-  category_label?: XmlElem<string>;
-  type?: XmlElem<string>;
-  type_name?: XmlElem<string>;
-  formulas?: XmlMultiElem<ClShapeDocumentFormula>;
-  textboxrect?: XmlElem<string>;
-  otherxml?: XmlElem<string>;
-  handles?: XmlMultiElem<ClShapeDocumentHandle>;
-  ico?: XmlElem<Binary>;
-  vml?: XmlElem<string>;
-  desc?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+interface ClShapeDocumentParams {
+  type: XmlElem<string>;
+  stroked: XmlElem<string>;
+  filled: XmlElem<string>;
+  coordsize: XmlElem<string>;
+  adj: XmlElem<string>;
+  path: XmlElem<string>;
 }
 
-type ClShapeDocument = XmlDocument & { TopElem: ClShapeDocumentTopElem; };
+type ClShapeDocumentTopElem = XmlTopElem & {
+  Doc: ClShapeDocument;
+  code: XmlElem<string>;
+  name: XmlElem<string>;
+  category: XmlElem<string>;
+  category_label: XmlElem<string>;
+  type: XmlElem<string>;
+  type_name: XmlElem<string>;
+  params: XmlElem<ClShapeDocumentParams>;
+  formulas: XmlMultiElem<ClShapeDocumentFormula>;
+  textboxrect: XmlElem<string>;
+  otherxml: XmlElem<string>;
+  handles: XmlMultiElem<ClShapeDocumentHandle>;
+  ico: XmlElem<Binary>;
+  vml: XmlElem<string>;
+  desc: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
+}
+
+type ClShapeDocument = XmlDocument & {
+  TopElem: ClShapeDocumentTopElem;
+};
