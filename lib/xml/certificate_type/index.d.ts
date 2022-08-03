2353b28bf48d1647376cf0f994966fdb41fc9ca0
umbrik <idumbrik@gmail.com>
2022-08-03 10:44:17 +0300

    ref certificate_type

--- lib/xml/certificate_type/index.d.ts
+++ lib/xml/certificate_type/index.d.ts
@@ -1,25 +1,28 @@
 interface CertificateTypeDocumentQualification {
-  qualification_id?: XmlElem<number>;
+  qualification_id: XmlElem<number>;
 }
 
 interface CertificateTypeDocumentCertificateType {
-  certificate_type_id?: XmlElem<number>;
+  certificate_type_id: XmlElem<number>;
 }
 
-type CertificateTypeDocumentTopElem = XmlTopElem & { Doc: CertificateTypeDocument } & 
+type CertificateTypeDocumentTopElem = XmlTopElem &
 CostCurrencyBase &
 AdminAccessBase &
 CustomElemsBase & {
-  name?: XmlElem<string>;
-  code?: XmlElem<string>;
-  education_org_id?: XmlElem<number>;
-  duration?: XmlElem<number>;
-  forever?: XmlElem<boolean>;
-  required_quantity?: XmlElem<number>;
-  qualifications?: XmlMultiElem<CertificateTypeDocumentQualification>;
-  certificate_types?: XmlMultiElem<CertificateTypeDocumentCertificateType>;
-  desc?: XmlElem<string>;
-  doc_info?: XmlElem<DocInfoBase>;
+  Doc: CertificateTypeDocument;
+  name: XmlElem<string>;
+  code: XmlElem<string>;
+  education_org_id: XmlElem<number>;
+  duration: XmlElem<number>;
+  forever: XmlElem<boolean>;
+  required_quantity: XmlElem<number>;
+  qualifications: XmlMultiElem<CertificateTypeDocumentQualification>;
+  certificate_types: XmlMultiElem<CertificateTypeDocumentCertificateType>;
+  desc: XmlElem<string>;
+  doc_info: XmlElem<DocInfoBase>;
 }
 
-type CertificateTypeDocument = XmlDocument & { TopElem: CertificateTypeDocumentTopElem; };
+type CertificateTypeDocument = XmlDocument & {
+  TopElem: CertificateTypeDocumentTopElem;
+};
