ad254d18f18fef2f568cdd4836cbe0caab24067c
umbrik <idumbrik@gmail.com>
2022-08-03 12:11:58 +0300

    ref course_module

--- lib/xml/course_module/index.d.ts
+++ lib/xml/course_module/index.d.ts
@@ -1,31 +1,41 @@
-type CourseModuleDocumentTopElem = XmlTopElem & { Doc: CourseModuleDocument } & 
+interface CourseModuleDocumentCmi5 {
+  moveon: XmlElem<string>;
+  launchmode: XmlElem<string>;
+  launchmethod: XmlElem<string>;
+}
+
+type CourseModuleDocumentTopElem = XmlTopElem &
 ObjectCodeNameBase &
 WebChecksBase &
 KnowledgePartsBase &
 CatalogListBase &
 CustomElemsBase &
 AdminAccessBase & {
-  desc?: XmlElem<string>;
-  eid?: XmlElem<string>;
-  type?: XmlElem<string>;
-  max_score?: XmlElem<number>;
-  mastery_score?: XmlElem<number>;
-  mastery_score_relative?: XmlElem<number>;
-  url?: XmlElem<string>;
-  assessment_id?: XmlElem<number>;
-  attempts_num?: XmlElem<number>;
-  set_status_side?: XmlElem<string>;
-  win_width?: XmlElem<number>;
-  win_height?: XmlElem<number>;
-  disp_scrolling?: XmlElem<boolean>;
-  version?: XmlElem<string>;
-  import_type?: XmlElem<string>;
-  activation_count?: XmlElem<number>;
-  launch_count?: XmlElem<number>;
-  access?: XmlElem<AccessDocBase>;
-  doc_info?: XmlElem<DocInfoBase>;
-  comment?: XmlElem<string>;
-  role_id?: XmlMultiElem<number>;
+  Doc: CourseModuleDocument;
+  desc: XmlElem<string>;
+  eid: XmlElem<string>;
+  type: XmlElem<string>;
+  max_score: XmlElem<number>;
+  mastery_score: XmlElem<number>;
+  mastery_score_relative: XmlElem<number>;
+  url: XmlElem<string>;
+  assessment_id: XmlElem<number>;
+  attempts_num: XmlElem<number>;
+  set_status_side: XmlElem<string>;
+  win_width: XmlElem<number>;
+  win_height: XmlElem<number>;
+  disp_scrolling: XmlElem<boolean>;
+  version: XmlElem<string>;
+  import_type: XmlElem<string>;
+  activation_count: XmlElem<number>;
+  launch_count: XmlElem<number>;
+  cmi5: XmlElem<CourseModuleDocumentCmi5>;
+  access: XmlElem<AccessDocBase>;
+  doc_info: XmlElem<DocInfoBase>;
+  comment: XmlElem<string>;
+  role_id: XmlMultiElem<number>;
 }
 
-type CourseModuleDocument = XmlDocument & { TopElem: CourseModuleDocumentTopElem; };
+type CourseModuleDocument = XmlDocument & {
+  TopElem: CourseModuleDocumentTopElem;
+};
