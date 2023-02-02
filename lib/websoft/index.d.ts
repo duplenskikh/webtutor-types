8d486aa4f7204d71b48c1f0f951ab5cf400d12bd
umbrik <693254+duplenskikh@users.noreply.github.com>
2023-02-03 01:40:11 +0300

    ref excel class

--- lib/websoft/index.d.ts
+++ lib/websoft/index.d.ts
@@ -4,29 +4,33 @@ declare namespace Websoft {
      * Библиотека для работы с документами Microsoft Office Excel
      */
     namespace Excel {
+
       /**
-       * Класс для работы с документом Microsoft Excel
+       * Описание границы ячейки
        */
-      class Document {
+      class Border {
         /**
-         * Количество страниц
+         * Цвет границы
          */
-        readonly WorkSheetsCount: number;
-
+        Color: string;
+        
         /**
-         * Создает новый лист
+         * Стиль границы
          */
-        AddWorksheet(): number;
-
-        /**
-         * Закрывает документ
-         */
-        Close(): 0 | 1;
-
-        /**
-         * Создает книгу Excel
-         */
-        CreateWorkBook(): 0 | 1;
+        LineStyle: "None"
+        | "DashDot"
+        | "DashDotDot"
+        | "Dashed"
+        | "Dotted"
+        | "Double"
+        | "Hair"
+        | "Medium"
+        | "MediumDashDot"
+        | "MediumDashDotDot"
+        | "MediumDashed"
+        | "SlantedDashDot"
+        | "Thick"
+        | "Thin";
 
         /**
          * Используется для получения последней произошедшей ошибки
@@ -37,102 +41,27 @@ declare namespace Websoft {
          * Используется для получения версии компонента
          */
         GetVersion(): string;
-
-        /**
-         * Получает значение листа по номеру
-         * @param index Порядковый номер листа
-         */
-        GetWorksheet(index: number): Worksheet;
-
-        /**
-         * Загружает данные в формате HTML из указанного файла
-         * @param filePath Путь до файла в формате HTML
-         * @param resourcesDirectoryName Название папки с ресурсами (изображения, стили и т.д.)
-         * 
-         */
-        LoadHtmlFile(filePath: string, resourcesDirectoryName: string): 0 | 1;
-
-        /**
-         * Загружает данные в формате HTML из строки
-         * @param htmlText Переменная, содержащаю строку в формате HTML
-         * @param resourcesDirectoryName Название папки с ресурсами (изображения, стили и т.д.)
-         * 
-         */
-        LoadHtmlString(htmlText: string, resourcesDirectoryName: string): 0 | 1;
-
-        /**
-         * Открывает файл Microsoft Excel
-         * @param filePath Путь до файла
-         */
-        Open(filePath: string): 1 | 0;
-
-        /**
-         * Сохраняет текущий документ
-         */
-        Save(): 1 | 0;
-
-        /**
-         * Сохраняет документ по указанному пути.
-         * Формат определяется по расширению имени файла в пути сохранения.
-         * Доступные форматы: CSV, ODS, TIFF, SVG, PDF, MHTML, HTML
-         * @param newPath Путь сохранения, включая имя файла
-         */
-        SaveAs(newPath: string): 1 | 0;
       }
 
       /**
-       * Лист книги Excel
+       * Класс для работы с границами ячейки.
        */
-      class Worksheet {
+      class Borders {
         /**
-         * Свойство для управления ячейками листа
+         * Получает описание границы указанного типа.
+         * @param string borderType
          */
-        Cells: Cells;
-
-        /**
-         * Наборы графиков
-         */
-        Charts: ChartsCollection;
-
-        /**
-         * Название листа
-         */
-        Name: string;
-
-        /**
-         * Параметры страницы
-         */
-        PageSetup: PageSetup;
-
-        /**
-         * Набор изображений листа
-         */
-        Pictures: Pictures;
-
-        /**
-         * Создает новый набор графиков
-         * @deprecated
-         * @param chartType Тип графика
-         * @param left Позиция по горизонтали
-         * @param top Позиция по вертикали
-         * @param width Ширинf
-         * @param height Высота
-         */
-        AddChart(
-          chartType: string,
-          left: number,
-          top: number,
-          width: number,
-          height: number
-        ): Chart;
-
-        /**
-         * Получает ячейку по указанному расположению
-         * (устаревшее, вместо данной функции нужно использовать Cells.GetCell())
-         * @param cellName Расположение
-         */
-        GetCell(cellName: string): Cell;
-
+        GetBorder(
+          borderType: "LeftBorder"
+          | "RightBorder"
+          | "TopBorder"
+          | "BottomBorder"
+          | "DiagonalDown"
+          | "DiagonalUp"
+          | "Vertical"
+          | "Horizontal"
+        ): Border;
+        
         /**
          * Используется для получения последней произошедшей ошибки
          */
@@ -140,6 +69,75 @@ declare namespace Websoft {
 
         /**
          * Используется для получения версии компонента
+         */
+        GetVersion(): string;
+        
+        /**
+         * Устанавливает стиль границ ячейки.
+         */
+        SetColor(color: string): void;
+        
+        /**
+         * Устанавливает стиль границ ячейки
+         */
+        SetStyle(
+          borderType: "None"
+          | "DashDot"
+          | "DashDotDot"
+          | "Dashed"
+          | "Dotted"
+          | "Double"
+          | "Hair"
+          | "Medium"
+          | "MediumDashDot"
+          | "MediumDashDotDot"
+          | "MediumDashed"
+          | "SlantedDashDot"
+          | "Thick"
+          | "Thin"
+        ): void;
+      }
+
+      class Cell {
+        /**
+         * Формула
+         */
+        Formula: string;
+        /**
+         * Значение в формате HTML
+         */
+        HtmlString: string;
+        /**
+         * Является ли ячейка объединенной с другими
+         */
+        readonly IsMerged: boolean;
+        /**
+         * Количество картинок в ячейке
+         */
+        readonly PicturesCount: number;
+        /**
+         * Стиль ячейки
+         */
+        readonly Style: Style;
+        /**
+         * Значение ячейки
+         */
+        Value: string;
+
+        /**
+         * Используется для получения последней произошедшей ошибки
+         * @returns Текст последней произошедшей ошибки
+         */
+        GetError(): string;
+        /**
+         * Получить картинку
+         * @param System.Int32 Порядковый номер изображения
+         * @returns Изображение
+         */
+        GetPicture(index: number): Drawing.Picture;
+        /**
+         * Используется для получения версии компонента
+         * @returns Версия компонента
          */
         GetVersion(): string;
       }
@@ -226,39 +224,318 @@ declare namespace Websoft {
         UnMerge(firstRow: number, firstColumn: number, totalRows: number, totalColumns: number): 1 | 0;
       }
 
-      class ChartsCollection {
+      class Column {}
 
+      class Columns {}
+
+      /**
+       * Класс для работы с документом Microsoft Excel
+       */
+      class Document {
+        /**
+         * Количество страниц
+         */
+        readonly WorkSheetsCount: number;
+
+        /**
+         * Создает новый лист
+         */
+        AddWorksheet(): number;
+
+        /**
+         * Закрывает документ
+         */
+        Close(): 0 | 1;
+
+        /**
+         * Создает книгу Excel
+         */
+        CreateWorkBook(): 0 | 1;
+
+        /**
+         * Используется для получения последней произошедшей ошибки
+         */
+        GetError(): string;
+
+        /**
+         * Используется для получения версии компонента
+         */
+        GetVersion(): string;
+
+        /**
+         * Получает значение листа по номеру
+         * @param index Порядковый номер листа
+         */
+        GetWorksheet(index: number): Worksheet;
+
+        /**
+         * Загружает данные в формате HTML из указанного файла
+         * @param filePath Путь до файла в формате HTML
+         * @param resourcesDirectoryName Название папки с ресурсами (изображения, стили и т.д.)
+         * 
+         */
+        LoadHtmlFile(filePath: string, resourcesDirectoryName: string): 0 | 1;
+
+        /**
+         * Загружает данные в формате HTML из строки
+         * @param htmlText Переменная, содержащаю строку в формате HTML
+         * @param resourcesDirectoryName Название папки с ресурсами (изображения, стили и т.д.)
+         * 
+         */
+        LoadHtmlString(htmlText: string, resourcesDirectoryName: string): 0 | 1;
+
+        /**
+         * Открывает файл Microsoft Excel
+         * @param filePath Путь до файла
+         */
+        Open(filePath: string): 1 | 0;
+
+        /**
+         * Сохраняет текущий документ
+         */
+        Save(): 1 | 0;
+
+        /**
+         * Сохраняет документ по указанному пути.
+         * Формат определяется по расширению имени файла в пути сохранения.
+         * Доступные форматы: CSV, ODS, TIFF, SVG, PDF, MHTML, HTML
+         * @param newPath Путь сохранения, включая имя файла
+         */
+        SaveAs(newPath: string): 1 | 0;
       }
+
+      class Font {}
 
       class PageSetup {
-
-      }
-
-      class Pictures {
-
-      }
-
-      class Cell {
         /**
-         * Значение ячейки
+         * Адаптировать содержимое по высоте под указанное количество страниц.
          */
-        Value: any;
+        FitToPagesTall: number;
+        
+        /**
+         * Адаптировать содержимое по ширине под указанное количество страниц.
+         */
+        FitToPagesWide: number;
+        
+        /**
+         * Ориентация страницы. Допустимые значения: "Landscape" или "Portrait".
+         */
+        Orientation: "Landscape" | "Portrait";
+
+        /**
+         * Размер страницы для печати
+         */
+        PaperSize: "PaperLetter"
+        | "PaperLetterSmall"
+        | "PaperTabloid"
+        | "PaperLedger"
+        | "PaperLegal"
+        | "PaperStatement"
+        | "PaperExecutive"
+        | "PaperA3"
+        | "PaperA4"
+        | "PaperA4Small"
+        | "PaperA5"
+        | "PaperB4"
+        | "PaperB5"
+        | "PaperFolio"
+        | "PaperQuarto"
+        | "Paper10x14"
+        | "Paper11x17"
+        | "PaperNote"
+        | "PaperEnvelope9"
+        | "PaperEnvelope10"
+        | "PaperEnvelope11"
+        | "PaperEnvelope12"
+        | "PaperEnvelope14"
+        | "PaperCSheet"
+        | "PaperDSheet"
+        | "PaperESheet"
+        | "PaperEnvelopeDL"
+        | "PaperEnvelopeC5"
+        | "PaperEnvelopeC3"
+        | "PaperEnvelopeC4"
+        | "PaperEnvelopeC6"
+        | "PaperEnvelopeC65"
+        | "PaperEnvelopeB4"
+        | "PaperEnvelopeB5"
+        | "PaperEnvelopeB6"
+        | "PaperEnvelopeItaly"
+        | "PaperEnvelopeMonarch"
+        | "PaperEnvelopePersonal"
+        | "PaperFanfoldUS"
+        | "PaperFanfoldStdGerman"
+        | "PaperFanfoldLegalGerman"
+        | "PaperISOB4"
+        | "PaperJapanesePostcard"
+        | "Paper9x11"
+        | "Paper10x11"
+        | "Paper15x11"
+        | "PaperEnvelopeInvite"
+        | "PaperLetterExtra"
+        | "PaperLegalExtra"
+        | "PaperTabloidExtra"
+        | "PaperA4Extra"
+        | "PaperLetterTransverse"
+        | "PaperA4Transverse"
+        | "PaperLetterExtraTransverse"
+        | "PaperSuperA"
+        | "PaperSuperB"
+        | "PaperLetterPlus"
+        | "PaperA4Plus"
+        | "PaperA5Transverse"
+        | "PaperJISB5Transverse"
+        | "PaperA3Extra"
+        | "PaperA5Extra"
+        | "PaperISOB5Extra"
+        | "PaperA2"
+        | "PaperA3Transverse"
+        | "PaperA3ExtraTransverse"
+        | "PaperJapaneseDoublePostcard"
+        | "PaperA6"
+        | "PaperJapaneseEnvelopeKaku2"
+        | "PaperJapaneseEnvelopeKaku3"
+        | "PaperJapaneseEnvelopeChou3"
+        | "PaperJapaneseEnvelopeChou4"
+        | "PaperLetterRotated"
+        | "PaperA3Rotated"
+        | "PaperA4Rotated"
+        | "PaperA5Rotated"
+        | "PaperJISB4Rotated"
+        | "PaperJISB5Rotated"
+        | "PaperJapanesePostcardRotated"
+        | "PaperJapaneseDoublePostcardRotated"
+        | "PaperA6Rotated"
+        | "PaperJapaneseEnvelopeKaku2Rotated"
+        | "PaperJapaneseEnvelopeKaku3Rotated"
+        | "PaperJapaneseEnvelopeChou3Rotated"
+        | "PaperJapaneseEnvelopeChou4Rotated"
+        | "PaperJISB6"
+        | "PaperJISB6Rotated"
+        | "Paper12x11"
+        | "PaperJapaneseEnvelopeYou4"
+        | "PaperJapaneseEnvelopeYou4Rotated"
+        | "PaperPRC16K"
+        | "PaperPRC32K"
+        | "PaperPRCBig32K"
+        | "PaperPRCEnvelope1"
+        | "PaperPRCEnvelope2"
+        | "PaperPRCEnvelope3"
+        | "PaperPRCEnvelope4"
+        | "PaperPRCEnvelope5"
+        | "PaperPRCEnvelope6"
+        | "PaperPRCEnvelope7"
+        | "PaperPRCEnvelope8"
+        | "PaperPRCEnvelope9"
+        | "PaperPRCEnvelope10"
+        | "PaperPRC16KRotated"
+        | "PaperPRC32KRotated"
+        | "PaperPRCBig32KRotated"
+        | "PaperPRCEnvelope1Rotated"
+        | "PaperPRCEnvelope2Rotated"
+        | "PaperPRCEnvelope3Rotated"
+        | "PaperPRCEnvelope4Rotated"
+        | "PaperPRCEnvelope5Rotated"
+        | "PaperPRCEnvelope6Rotated"
+        | "PaperPRCEnvelope7Rotated"
+        | "PaperPRCEnvelope8Rotated"
+        | "PaperPRCEnvelope9Rotated"
+        | "PaperPRCEnvelope10Rotated"
+        | "PaperB3"
+        | "PaperBusinessCard";
+
+        /**
+         * Используется для получения последней произошедшей ошибки
+         * @returns Текст последней произошедшей ошибки
+         */
+        GetError(): string;
+        /**
+         * Используется для получения версии компонента
+         * @returns Версия компонента
+         */
+        GetVersion(): string;
       }
 
-      class Chart {
+      class Range {}
 
+      class Row {}
+
+      class Rows {}
+
+      class Style {}
+
+      /**
+       * Лист книги Excel
+       */
+      class Worksheet {
+        /**
+         * Свойство для управления ячейками листа
+         */
+        Cells: Cells;
+
+        /**
+         * Наборы графиков
+         */
+        Charts: ChartsCollection;
+
+        /**
+         * Название листа
+         */
+        Name: string;
+
+        /**
+         * Параметры страницы
+         */
+        PageSetup: PageSetup;
+
+        /**
+         * Набор изображений листа
+         */
+        Pictures: Pictures;
+
+        /**
+         * Создает новый набор графиков
+         * @deprecated
+         * @param chartType Тип графика
+         * @param left Позиция по горизонтали
+         * @param top Позиция по вертикали
+         * @param width Ширинf
+         * @param height Высота
+         */
+        AddChart(
+          chartType: string,
+          left: number,
+          top: number,
+          width: number,
+          height: number
+        ): Chart;
+
+        /**
+         * Получает ячейку по указанному расположению
+         * (устаревшее, вместо данной функции нужно использовать Cells.GetCell())
+         * @param cellName Расположение
+         */
+        GetCell(cellName: string): Cell;
+
+        /**
+         * Используется для получения последней произошедшей ошибки
+         */
+        GetError(): string;
+
+        /**
+         * Используется для получения версии компонента
+         */
+        GetVersion(): string;
       }
 
-      class Columns {
+      class ChartsCollection {}
 
-      }
+      class Pictures {}
 
-      class Rows {
+      class Chart {}
 
-      }
-
-      class Range {
-
+      namespace Drawing {
+        class Picture {}
       }
     }
   }
