80c36188c0bab0104f2884b5dd30be2adbf8af61
umbrik <idumbrik@gmail.com>
2023-08-01 09:59:50 +0300

    Upd

--- ci/lint.ts
+++ ci/lint.ts
@@ -5,15 +5,32 @@ function main() {
   const diffOutput = execSync("git diff origin/main --name-only *.d.ts");
   const files = diffOutput.toString().split("\n").filter(Boolean);
 
-  console.log(`Найдено измененных файлов ${files.length}:\n${files.join("\n")}`);
+  console.log(`Найдено измененных файлов ${files.length}`);
 
-  for (const file of files) {
+  if (files.length > 10) {
     try {
-      execSync(`npx eslint ./${file} --rule '{"@typescript-eslint/no-explicit-any": "error"}'`, {
+      execSync("npx eslint *.d.ts", {
         stdio: ["ignore", process.stdout, process.stderr]
       });
-    } catch (error) {
+    } catch {
       setTimeout(() => process.exit(1));
+    }
+  } else {
+    console.log(`Измененные файлы:\n${files.join("\n")}`);
+
+    let i = 0;
+
+    for (const file of files) {
+      console.log(`Обрабатывается файл ${i}`);
+      i++;
+
+      try {
+        execSync(`npx eslint ./${file} --rule '{"@typescript-eslint/no-explicit-any": "error"}'`, {
+          stdio: ["ignore", process.stdout, process.stderr]
+        });
+      } catch {
+        setTimeout(() => process.exit(1));
+      }
     }
   }
 }
