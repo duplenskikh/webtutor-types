984c70e48211b37e45e9a5c1ba018adf2677adfe
Markvart Kirill <markvart.kirill@yandex.ru>
2023-08-22 21:58:56 +0300

    tests: добавил тесты функций работы с массивами

--- tests/core.errors.ts
+++ tests/core.errors.ts
@@ -1,2 +1,96 @@
+import {arrayFunctionsNegativeTestNumberValue, arrayFunctionsNegativeTestStringValue, xmlDocumentForTesting} from "./mocks/arrayFunctions"
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'string'.
 ClipboardFormatAvailable(1);
+
+
+ArrayCount(arrayFunctionsNegativeTestStringValue);
+
+ArrayDirect(arrayFunctionsNegativeTestStringValue);
+
+ArrayExtract(arrayFunctionsNegativeTestStringValue, "This");
+ArrayExtract(arrayFunctionsNegativeTestNumberValue, "This.option1");
+
+ArrayExtractKeys(arrayFunctionsNegativeTestStringValue, "This");
+ArrayExtractKeys(arrayFunctionsNegativeTestNumberValue, "This.option1");
+ArrayExtractKeys(xmlDocumentForTesting.TopElem.name, "This.name");
+
+ArrayFind(arrayFunctionsNegativeTestStringValue, "This == 2");
+ArrayFind(arrayFunctionsNegativeTestNumberValue, "This.option1 == 1");
+ArrayFind(xmlDocumentForTesting.TopElem, "This.name == \"testValue\"");
+
+ArrayFirstElem(arrayFunctionsNegativeTestStringValue);
+ArrayFirstElem(arrayFunctionsNegativeTestNumberValue);
+ArrayFirstElem(xmlDocumentForTesting.TopElem);
+
+ArrayIntersect(arrayFunctionsNegativeTestStringValue, arrayFunctionsNegativeTestNumberValue);
+ArrayIntersect(arrayFunctionsNegativeTestNumberValue, arrayFunctionsNegativeTestStringValue, "This.option1");
+ArrayIntersect(xmlDocumentForTesting.TopElem.name, arrayFunctionsNegativeTestNumberValue, "This.name", "This.option1");
+
+ArrayMax(arrayFunctionsNegativeTestStringValue, "This");
+ArrayMax(arrayFunctionsNegativeTestNumberValue, "This.option1");
+ArrayMax(xmlDocumentForTesting, "This.plan_date");
+
+ArrayMerge(arrayFunctionsNegativeTestStringValue, "This", ";");
+ArrayMerge(arrayFunctionsNegativeTestNumberValue, "This.option1", ";");
+ArrayMerge(xmlDocumentForTesting.DocID, "This.plan_date", ";");
+
+ArrayMin(arrayFunctionsNegativeTestStringValue, "This");
+ArrayMin(arrayFunctionsNegativeTestNumberValue, "This.option1");
+ArrayMin(xmlDocumentForTesting.TopElem.Doc, "This.plan_date");
+
+ArrayOptFind(arrayFunctionsNegativeTestStringValue, "This < 1");
+ArrayOptFind(arrayFunctionsNegativeTestNumberValue, "This.option1 > 2");
+ArrayOptFind(xmlDocumentForTesting.TopElem, "This.name != \"testValue\"");
+
+ArrayOptFindByKey(arrayFunctionsNegativeTestStringValue, 2);
+ArrayOptFindByKey(arrayFunctionsNegativeTestNumberValue, 1, "option1");
+ArrayOptFindByKey(xmlDocumentForTesting.TopElem, "testValue");
+
+ArrayOptFindBySortedKey(arrayFunctionsNegativeTestStringValue, 2);
+ArrayOptFindBySortedKey(arrayFunctionsNegativeTestNumberValue, 1, "option1");
+ArrayOptFindBySortedKey(xmlDocumentForTesting, "testValue");
+
+ArrayOptFirstElem(arrayFunctionsNegativeTestStringValue);
+ArrayOptFirstElem(arrayFunctionsNegativeTestNumberValue, 1);
+ArrayOptFirstElem(xmlDocumentForTesting.TopElem.person_id, "testValue");
+
+ArrayOptMax(arrayFunctionsNegativeTestStringValue, "This");
+ArrayOptMax(arrayFunctionsNegativeTestNumberValue, "This.option1");
+ArrayOptMax(xmlDocumentForTesting.TopElem.person_id, "This.plan_date");
+
+ArrayOptMin(arrayFunctionsNegativeTestStringValue, "This");
+ArrayOptMin(arrayFunctionsNegativeTestNumberValue, "This.option1");
+ArrayOptMin(xmlDocumentForTesting.TopElem.personnel_reserve_id, "This.plan_date");
+
+ArrayRange(arrayFunctionsNegativeTestStringValue, 1, 3);
+ArrayRange(arrayFunctionsNegativeTestNumberValue, 0, 10);
+ArrayRange(xmlDocumentForTesting.TopElem, 3, 5);
+
+ArraySelect(arrayFunctionsNegativeTestStringValue, "This < 1");
+ArraySelect(arrayFunctionsNegativeTestNumberValue, "This.option1 > 2");
+ArraySelect(xmlDocumentForTesting.TopElem, "This.name != \"testValue\"");
+
+ArraySelectAll(arrayFunctionsNegativeTestStringValue);
+ArraySelectAll(arrayFunctionsNegativeTestNumberValue);
+ArraySelectAll(xmlDocumentForTesting);
+
+ArraySelectByKey(arrayFunctionsNegativeTestStringValue, 1, "This");
+ArraySelectByKey(arrayFunctionsNegativeTestNumberValue, 1, "option1");
+ArraySelectByKey(xmlDocumentForTesting.TopElem.name, "testValue", "name");
+
+ArraySelectBySortedKey(arrayFunctionsNegativeTestStringValue, 1, "a");
+ArraySelectBySortedKey(arrayFunctionsNegativeTestNumberValue, 2, "option1");
+ArraySelectBySortedKey(xmlDocumentForTesting.TopElem.Name, "testValue", "name");
+
+ArraySelectDistinct(arrayFunctionsNegativeTestStringValue, "1");
+ArraySelectDistinct(arrayFunctionsNegativeTestNumberValue, "testValue");
+ArraySelectDistinct(xmlDocumentForTesting.TopElem.comment.ForeignArray, "testValue");
+
+ArraySort(arrayFunctionsNegativeTestStringValue, "This", "-");
+ArraySort(arrayFunctionsNegativeTestNumberValue, "option1", "-");
+ArraySort(xmlDocumentForTesting.TopElem.person_id.ForeignElem, "name", "+");
+
+ArraySum(arrayFunctionsNegativeTestStringValue, "This");
+ArraySum(arrayFunctionsNegativeTestNumberValue, "option1");
+ArraySum(xmlDocumentForTesting.TopElem.person_id, "id");
--- tests/core.types.ts
+++ tests/core.types.ts
@@ -1,1 +1,148 @@
+import {testArrayOfObjects, testPrimitivesArray, xmlDocumentForTesting} from "./mocks/arrayFunctions";
+
 CheckPamMd5("source_string", "checksum");
+
+ArrayCount(testPrimitivesArray);
+ArrayCount(testArrayOfObjects);
+ArrayCount(xmlDocumentForTesting.TopElem.tasks);
+
+ArrayDirect(testPrimitivesArray);
+ArrayDirect(testArrayOfObjects);
+
+ArrayExtract(testPrimitivesArray, "This");
+ArrayExtract(testArrayOfObjects, "This.option1");
+ArrayExtract(xmlDocumentForTesting.TopElem.tasks, "This.name");
+
+ArrayExtractKeys(testPrimitivesArray, "This");
+ArrayExtractKeys(testArrayOfObjects, "This.option1");
+ArrayExtractKeys(xmlDocumentForTesting.TopElem.tasks, "This.name");
+
+ArrayFind(testPrimitivesArray, "This == 2");
+ArrayFind(testArrayOfObjects, "This.option1 == 1");
+const arrayFindXmlResultData = ArrayFind(xmlDocumentForTesting.TopElem.tasks, "This.name == \"testValue\"");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayFindXmlResultData.OptChild("testValue");
+
+ArrayFirstElem(testPrimitivesArray);
+ArrayFirstElem(testArrayOfObjects);
+const arrayFirstElemXmlResultData = ArrayFirstElem(xmlDocumentForTesting.TopElem.tasks);
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayFirstElemXmlResultData.OptChild("testValue");
+
+ArrayIntersect(testPrimitivesArray, testArrayOfObjects);
+ArrayIntersect(testArrayOfObjects, testPrimitivesArray, "This.option1");
+const arrayIntersectXmlResultData = ArrayIntersect(xmlDocumentForTesting.TopElem.tasks, testArrayOfObjects, "This.name", "This.option1");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayIntersectXmlResultData[0].OptChild("testValue");
+
+ArrayMax(testPrimitivesArray, "This");
+ArrayMax(testArrayOfObjects, "This.option1");
+const arrayMaxXmlResultData = ArrayMax(xmlDocumentForTesting.TopElem.tasks, "This.plan_date");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayMaxXmlResultData.OptChild("testValue");
+
+ArrayMerge(testPrimitivesArray, "This", ";");
+ArrayMerge(testArrayOfObjects, "This.option1", ";");
+ArrayMerge(xmlDocumentForTesting.TopElem.tasks, "This.plan_date", ";");
+
+ArrayMin(testPrimitivesArray, "This");
+ArrayMin(testArrayOfObjects, "This.option1");
+const arrayMinXmlResultData = ArrayMin(xmlDocumentForTesting.TopElem.tasks, "This.plan_date");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayMinXmlResultData.OptChild("testValue");
+
+ArrayOptFind(testPrimitivesArray, "This < 1");
+ArrayOptFind(testArrayOfObjects, "This.option1 > 2");
+const arrayOptFindXmlResultData = ArrayOptFind(xmlDocumentForTesting.TopElem.tasks, "This.name != \"testValue\"");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayOptFindXmlResultData?.OptChild("testValue");
+
+ArrayOptFindByKey(testPrimitivesArray, 2);
+ArrayOptFindByKey(testArrayOfObjects, 1, "option1");
+const arrayOptFindByKeyXmlResultData = ArrayOptFindByKey(xmlDocumentForTesting.TopElem.tasks, "testValue");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayOptFindByKeyXmlResultData?.OptChild("testValue");
+
+ArrayOptFindBySortedKey(testPrimitivesArray, 2);
+ArrayOptFindBySortedKey(testArrayOfObjects, 1, "option1");
+const arrayOptFindBySortedKeyXmlResultData = ArrayOptFindBySortedKey(xmlDocumentForTesting.TopElem.tasks, "testValue");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayOptFindBySortedKeyXmlResultData?.OptChild("testValue");
+
+ArrayOptFirstElem(testPrimitivesArray);
+ArrayOptFirstElem(testArrayOfObjects, 1);
+const arrayOptFirstElemXmlResultData = ArrayOptFirstElem(xmlDocumentForTesting.TopElem.tasks);
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayOptFirstElemXmlResultData?.OptChild("testValue");
+const arrayOptFirstElemXmlResultDataDefaultValue = ArrayOptFirstElem(xmlDocumentForTesting.TopElem.tasks, "testValue");
+if(arrayOptFirstElemXmlResultDataDefaultValue !== "testValue"){
+  // если нам вернулось не значение по умолчанию то должен быть XmlElem
+  arrayOptFirstElemXmlResultDataDefaultValue?.OptChild("testValue");
+}
+
+ArrayOptMax(testPrimitivesArray, "This");
+ArrayOptMax(testArrayOfObjects, "This.option1");
+const arrayOptMaxXmlResultData = ArrayOptMax(xmlDocumentForTesting.TopElem.tasks, "This.plan_date");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayOptMaxXmlResultData?.OptChild("testValue");
+
+ArrayOptMin(testPrimitivesArray, "This");
+ArrayOptMin(testArrayOfObjects, "This.option1");
+const arrayOptMinXmlResultData = ArrayOptMin(xmlDocumentForTesting.TopElem.tasks, "This.plan_date");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayOptMinXmlResultData?.OptChild("testValue");
+
+ArrayRange(testPrimitivesArray, 1, 3);
+ArrayRange(testArrayOfObjects, 0, 10);
+const arrayRangeXmlResultData = ArrayRange(xmlDocumentForTesting.TopElem.tasks, 3, 5);
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arrayRangeXmlResultData[0].OptChild("testValue");
+
+ArraySelect(testPrimitivesArray, "This < 1");
+ArraySelect(testArrayOfObjects, "This.option1 > 2");
+const arraySelectXmlResultData = ArraySelect(xmlDocumentForTesting.TopElem.tasks, "This.name != \"testValue\"");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arraySelectXmlResultData[0].OptChild("testValue");
+
+ArraySelectAll(testPrimitivesArray);
+ArraySelectAll(testArrayOfObjects);
+const arraySelectAllXmlResultData = ArraySelectAll(xmlDocumentForTesting.TopElem.tasks);
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arraySelectAllXmlResultData[0].OptChild("testValue");
+
+ArraySelectByKey(testPrimitivesArray, 1, "This");
+ArraySelectByKey(testArrayOfObjects, 1, "option1");
+const arraySelectByKeyXmlResultData = ArraySelectByKey(xmlDocumentForTesting.TopElem.tasks, "testValue", "name");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arraySelectByKeyXmlResultData[0].OptChild("testValue");
+
+ArraySelectBySortedKey(testPrimitivesArray, 1, "a");
+ArraySelectBySortedKey(testArrayOfObjects, 2, "option1");
+const arraySelectBySortedKeyXmlResultData = ArraySelectBySortedKey(xmlDocumentForTesting.TopElem.tasks, "testValue", "name");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arraySelectBySortedKeyXmlResultData[0].OptChild("testValue");
+
+ArraySelectDistinct(testPrimitivesArray, "1");
+ArraySelectDistinct(testArrayOfObjects, "testValue");
+const arraySelectDistinctXmlResultData = ArraySelectDistinct(xmlDocumentForTesting.TopElem.tasks, "testValue");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arraySelectDistinctXmlResultData[0].OptChild("testValue");
+
+ArraySort(testPrimitivesArray, "This", "-");
+ArraySort(testArrayOfObjects, "option1", "-");
+const arraySortXmlResultData = ArraySort(xmlDocumentForTesting.TopElem.tasks, "name", "+");
+// проверка на то что мы получаем XmlElem, к которому можем обратиться
+arraySortXmlResultData[0].OptChild("testValue");
+
+ArraySum(testPrimitivesArray, "This");
+ArraySum(testArrayOfObjects, "option1");
+ArraySum(xmlDocumentForTesting.TopElem.tasks, "id");
+
+ArrayUnion(testPrimitivesArray, testArrayOfObjects);
+ArrayUnion(testArrayOfObjects, xmlDocumentForTesting);
+ArrayUnion(xmlDocumentForTesting.TopElem.tasks, testArrayOfObjects, testArrayOfObjects);
+
+IsArray(testPrimitivesArray);
+IsArray(testArrayOfObjects);
+IsArray(xmlDocumentForTesting.TopElem.tasks);
+IsArray(1);
