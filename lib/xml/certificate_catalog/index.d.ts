292bb52cefbefb9871c1a19cb4f7b295d62bfb3a
umbrik <693254+duplenskikh@users.noreply.github.com>
2024-02-12 20:04:01 +0100

    Update certificate_catalog document

--- lib/xml/certificate_catalog/index.d.ts
+++ lib/xml/certificate_catalog/index.d.ts
@@ -4,12 +4,12 @@ AdminAccessBase & {
   id: XmlElem<number>;
   serial: XmlElem<string>;
   number: XmlElem<string>;
-  type_id: XmlElem<number>;
+  type_id: XmlElem<number, CertificateTypeCatalogDocumentTopElem>;
   type_name: XmlElem<string>;
-  education_org_id: XmlElem<number>;
-  event_id: XmlElem<number>;
-  qualification_id: XmlElem<number>;
-  person_id: XmlElem<number>;
+  education_org_id: XmlElem<number, EducationOrgCatalogDocumentTopElem>;
+  event_id: XmlElem<number, EventCatalogDocumentTopElem>;
+  qualification_id: XmlElem<number, QualificationCatalogDocumentTopElem>;
+  person_id: XmlElem<number, CollaboratorCatalogDocumentTopElem>;
   valid: XmlElem<boolean>;
   delivery_date: XmlElem<Date>;
   expire_date: XmlElem<Date>;
