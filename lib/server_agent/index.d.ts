2d91b42add2557b2395281968491396b8a735a99
umbrik <idumbrik@gmail.com>
2024-02-23 00:14:14 +0100

    Move all Xml types to parent folder

---
+++ lib/server_agent/index.d.ts
@@ -0,0 +1,23 @@
+/**
+ * Объект параметров агента.
+ */
+declare var Param: Object;
+/**
+ * Id объекта над которым выполняется агент.
+ */
+declare var OBJECT_ID: number;
+declare var OBJECTS_ID_STR: string;
+/**
+ * Флаг исполнения кода на клиенте.
+ */
+declare var LdsIsClient: boolean;
+/**
+ * Флаг исполнения кода на сервере.
+ */
+declare var LdsIsServer: boolean;
+
+declare var oData: unknown;
+
+declare const LdsCurUserID: number;
+
+declare const curParams: Object;
--- lib/xml/index.d.ts
+++ lib/xml/index.d.ts
@@ -1289,27 +1289,3 @@ interface ObjectTypeBase {
   object_id: XmlElem<number>;
   object_name: XmlElem<string>;
 }
-
-/**
- * Объект параметров агента.
- */
-declare var Param: Object;
-/**
- * Id объекта над которым выполняется агент.
- */
-declare var OBJECT_ID: number;
-declare var OBJECTS_ID_STR: string;
-/**
- * Флаг исполнения кода на клиенте.
- */
-declare var LdsIsClient: boolean;
-/**
- * Флаг исполнения кода на сервере.
- */
-declare var LdsIsServer: boolean;
-
-declare var oData: unknown;
-
-declare const LdsCurUserID: number;
-
-declare const curParams: Object;
