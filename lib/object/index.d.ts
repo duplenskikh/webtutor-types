fa33e410e0e6fcd2faa67fd2e7668330e438e7a7
Ivan Duplenskikh <693254+duplenskikh@users.noreply.github.com>
2024-07-02 19:22:44 +0200

    Переработка методов Object

--- lib/object/index.d.ts
+++ lib/object/index.d.ts
@@ -1,51 +1,74 @@
 /**
- * Конструктор объекта javascript. Создает новый объект.
- * По умолчанию для объекта определен атрибут HasProperty и методы
- * AddProperty , SetProperty , GetProperty , GetOptProperty .
+ * Конструктор объекта javascript.
+ *
+ * Создает новый объект.
+ *
+ * По умолчанию для объекта определен атрибуты и методы:
+ * - {@link HasProperty}
+ * - {@link AddProperty}
+ * - {@link SetProperty}
+ * - {@link GetProperty}
+ * - {@link GetOptProperty}
  */
 interface Object {
   [key: string | number]: unknown;
 
   /**
-   * Выдает true, если атрубут с заданным наименованием существует,
-   * и false - если не существует. Смотри также GetOptProperty,
-   * GetOptProperty и SetProperty.
+   * Выдает true, если атрубут с заданным наименованием существует, и false - если не существует.
+   *
+   * Смотри также {@link GetOptProperty}, {@link GetOptProperty} и {@link SetProperty}.
    * @param key - Наименование атрибута объекта.
    */
   HasProperty(key: string | number): boolean;
 
   /**
-   * Добавляет в объект (javascript object) новый атрибут, и присваивает ему значение.
+   * Добавляет в объект новый атрибут, и присваивает ему значение.
    * @param {string} key - Наименование атрибута.
-   * @param name - Значение атрибута (variant).
+   * @param name - Значение атрибута.
    */
   AddProperty(key: string | number, name: unknown): void;
 
   /**
    * Возвращает значение атрибута объекта. Если атрубут отсутствует, выдает undefined.
    * @param {string} key - Наименование атрибута объекта.
-   * @param defaultValue - Значение по умолчанию, возращаемое в случае
-   * отстутвия атрибута (Any). Необязятельный аргумент. По умолчанию равен undefined.
+   * @param {unknown} [defaultValue=undefined] - Значение по умолчанию, возращаемое в случае отсутствия атрибута.
    */
-  GetOptProperty<T>(key: string | number): T | undefined;
-  GetOptProperty<T, K>(key: string | number, defaultValue?: K): T | K;
+  GetOptProperty<O, K extends string>(
+    this: O,
+    key: K
+  ): K extends keyof O ? O[K] : undefined;
+
+  GetOptProperty<O, K extends string, D>(
+    this: O,
+    key: K,
+    defaultValue: D
+  ): K extends keyof O ? O[K] : D;
 
   /**
    * Выдает значение атрибута объекта. Если атрибут отсутствует, выдает ошибку.
-   * Смотри также {@link GetOptProperty}() и SetProperty.
-   * @param key - Наименование атрибута объекта.
-   * @example AppConfig.GetProperty( 'alt-app-name' );
+   *
+   * Смотри также {@link GetOptProperty}() и {@link SetProperty}().
+   * @param {string} key - Наименование атрибута объекта.
+   * @example
+   * ```
+   * AppConfig.GetProperty("alt-app-name");
+   * ```
    * @throws {Error}
    */
-  GetProperty(key: string | number): unknown | never;
+  GetProperty<O, K extends string>(
+    this: O,
+    key: K
+  ): K extends keyof O ? O[K] : never;
 
   /**
-   * Устанавливает значение атрибута объекта. Если атрубут отсутствует,
-   * добавляет его. Смотри также {@link GetOptProperty}() и GetProperty.
+   * Устанавливает значение атрибута объекта.
+   * Если атрубут отсутствует, добавляет его.
+   *
+   * Смотри также {@link GetOptProperty}() и {@link GetProperty}().
    * @param key - Наименование атрибута объекта.
    * @param value - Значение атрибута объекта.
    */
-  SetProperty(key: string | number, value: unknown): void;
+  SetProperty<T>(key: string | number, value: T): void;
 }
 
 interface ObjectConstructor {
