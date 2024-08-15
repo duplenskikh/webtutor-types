be6b680229b7190ece780c4af70d216543544236
Ivan Duplenskikh <693254+duplenskikh@users.noreply.github.com>
2024-08-15 21:11:40 +0200

    Исправлены возвращаемые типы для методов GetChildByKey и ObtainChildByKey (#87)
    * Исправлены возвращаемые типы для методов GetChildByKey и ObtainChildByKey
    Теперь методы GetChildByKey и ObtainChildByKey возвращают XmlElem<T> вместо XmlElem<unknown>
    
    * Обновление версии
    
    * Перенесены тесты

--- tests/core.types.ts
+++ tests/core.types.ts
@@ -202,9 +202,3 @@ propertyValue2;
 DecodeCharset("teststring", "cp-866");
 EncodeCharset("teststring1", "windows-1251");
 EncodeCharset("teststring2", "windows-1252");
-
-const doc = tools.open_doc<CollaboratorDocument>(1);
-
-if (doc != undefined) {
-  doc.TopElem.firstname.Value = null;
-}
